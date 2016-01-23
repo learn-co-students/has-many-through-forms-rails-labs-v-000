5c472961b047c2f751ff6665f8fa388e9f0ebdab
jakebrady5 <jakebrady5@gmail.com>
2016-01-22 21:47:32 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def user_list
+    list = []
+    self.users.each do |a|
+      list << a
+    end
+    list.uniq
+  end
 
 end

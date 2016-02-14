3330882cc34d06a71b1577690f8fa0e4195abb14
plai217 <philiplai217@gmail.com>
2016-02-13 22:22:10 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+Category.destroy_all
+PostCategory.destroy_all
+
+Category.create([
+  {name: "Category 1"},
+  {name: "Category 2"}
+])
+
+Post.create([
+  {title: "Post 1", content: "Content BBB Post 1"},
+  {title: "Post 2", content: "Content AAA Post 2"},
+  {title: "Post 3", content: "Content DDD Post 3"},
+  {title: "Post 4", content: "Content ZZZ Post 4"}
+])
+
+PostCategory.create([
+  {post_id: 1,category_id: 1},
+  {post_id: 2,category_id: 2},
+  {post_id: 3,category_id: 1},
+  {post_id: 4,category_id: 2},
+  {post_id: 4,category_id: 1},
+  {post_id: 1,category_id: 2}
+])

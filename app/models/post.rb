6b3d4f37ab313ba85eda97cfe9ce8bb6febc74b5
Adam King <adam.king0126@gmail.com>
2016-02-28 12:37:38 -0600

    stuck on collection_select

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments
+
+  def commentors
+    self.comments.map{|c| c.user}.uniq
+  end
+
+  def comments_attributes=(attributes)
+    self.comments.build(attributes["0"])
+  end
 
 
 end

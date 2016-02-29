a28b515e5d325c82de7dd935937b2a9f79c25e98
Adam King <adam.king0126@gmail.com>
2016-02-29 11:47:56 -0600

    up to date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
-require 'pry'
 class PostsController < ApplicationController
-
-  #def show
-  #  @post = Post.find(params[:id])
-  #end
+  def show
+    @post = Post.find(params[:id])
+  end
 
   def index
     @posts = Post.all
@@ -18,16 +16,15 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to @post
-  end
+  # def update
+  #   @post = Post.find(params[:id])
+  #   @post.update(post_params)
+  #   redirect_to @post
+  # end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
-    #params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comments_attributes: [:user_id, :content, user_attributes: [:username]])
   end
 end

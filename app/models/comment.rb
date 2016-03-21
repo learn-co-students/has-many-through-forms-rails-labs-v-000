58938a194215e28d67daa3f919b268ce7f92be81
jsgoldb <jgoldb@yahoo.com>
2016-03-21 14:37:29 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :no_new_user
+
+  def no_new_user(user_attributes)
+    user_attributes[:username].blank? 
+  end
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |attr|
+      category = Category.find_or_create_by(attr)
+      self.categories << category
+    end
+  end
+
+  def unique_users 
+    self.comments.map {|comment| comment.user }.uniq
+  end
+
+
+
 
 end

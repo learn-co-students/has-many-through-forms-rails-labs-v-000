814df298917306d2240a2f258f41df9f07c63684
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-24 19:08:21 +0000

    got it working my way

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,12 @@ class Post < ActiveRecord::Base
   def commented_users
     self.users.uniq
   end
+  
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
 end

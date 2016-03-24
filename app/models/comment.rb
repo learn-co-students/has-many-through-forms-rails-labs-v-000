cae636239f4cafe609b9215e98025a1f3b06db3b
Louiefigz <figueroa.aaron@gmail.com>
2016-03-23 22:20:16 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(params)
+   unless params[:username] == ""
+     self.user = User.find_or_create_by(username: params[:username])
+   end
+ end
 end

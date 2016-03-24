beeea7eccc3814b8221fe585601a8d9c6267f456
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-24 18:26:26 +0000

    passes posts spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  def user_name 
+    self.user.username
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def comment_count 
+    self.comments.count
+  end
+  
+  def commented_users
+    self.users.uniq
+  end
 
 end

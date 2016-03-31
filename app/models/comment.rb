058c6a194c948f8a25f66a291b24dc7ee45a339e
shinyrachel <rachel.kory@gmail.com>
2016-03-31 13:26:16 -0400

    posts spec passing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :already_exists
+
+  def user_name
+    user.username
+  end
+
 end

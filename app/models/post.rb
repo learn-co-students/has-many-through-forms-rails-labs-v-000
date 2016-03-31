024ab656b47c08758bdb5c0f9471b140411ae63b
Tom Johns <tommyskies@hotmail.com>
2016-03-31 17:08:44 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :is_blank?
+
+  def categories_attributes=(atts)
+  	atts.values.each do |att|
+  		category = Category.find_or_create_by(att)
+  		self.categories << category unless self.categories.include?(category)
+  	end 
+  end
+  
+  private 
+  	 def is_blank?(category_attribute)
+  	 	binding.pry
+  	 	category_attribute['name'].blank?
+  	 end
+  	 	
 
 end

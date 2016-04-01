b894ee99bb77ae14ab68f2f02c80074e96e8303f
brianmenard <useyourbrian@gmail.com>
2016-04-01 15:24:20 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+  
+   def user_attributes=(att)
+    	if !self.user_id.present?
+   		  att['username'] = 'Anonymous' unless !att['username'].blank?
+   		  user = User.find_or_create_by(att)
+   		  self.user = user
+     	end
+   end
+  
 end

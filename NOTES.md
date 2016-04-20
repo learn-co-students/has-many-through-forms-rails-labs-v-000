ccfd85ea28d238fa811839b3e7afb2d2ea3e4b22
agdavid <antonio.g.david@gmail.com>
2016-04-20 16:45:57 -0400

    Done.

---
+++ NOTES.md
@@ -0,0 +1,18 @@
+posts/new
+  - checkboxes for existing categories
+  - text field for new category >> find or create by name in model
+
+posts/show
+  - title
+  - content
+  - all comments
+  - unique users
+  - form for new comment
+    - drop down to select user
+    - text field for new user >> associate with post
+
+users/show
+  [x] all posts with comments >> link posts/show
+
+categories/show
+  [x] all posts of category
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,20 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(categories_hashes) #hash is {"0" => {"name" => "NAME INPUT IF ANY"}}
+    categories_hashes.each do |i, category_attributes| #i => "0" , attribute => {"name" => "NAME INPUT IF ANY"}
+      if category_attributes[:name].present? # checking "NAME INPUT IF ANY"
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(category: category)
+        end
+      end
+    end
+  end
+  
 end

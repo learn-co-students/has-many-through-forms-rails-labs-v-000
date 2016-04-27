76163285c62ed8a53cfdbadb026e15cefcf830c1
Tyler Smith <oats000@hotmail.com>
2016-04-26 23:03:15 -0400

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
+
+  def user_attributes=(user_attributes)
+    user = User.find_or_create_by(user_attribute)
+    self.user = user
+  end
+
 end

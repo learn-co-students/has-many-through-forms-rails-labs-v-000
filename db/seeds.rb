1e952e419078b9b192c764b7d72871c45b38cef4
zacscodingclub <zbaston@gmail.com>
2016-05-12 21:11:46 -0500

    8/12 tests working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+["CoolGuy1","CoolGuy2","CoolGuy5"].each do |user| 
+  User.create(username: user)
+end
--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -9,14 +9,9 @@ describe 'categories', type: 'feature' do
   end
 
   describe 'show page' do
-
     it 'should display all of the related posts' do
       visit category_path(@cool)
       expect(page).to have_link(@post.title, href: post_path(@post))
     end
-
   end
-
-
-
 end

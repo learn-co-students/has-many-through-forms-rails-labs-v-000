d2f0bd52bdd036b6d06562633dc27445b45db150
lawrend <douglaslawrence314@gmail.com>
2016-06-05 20:08:07 -0700

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+dave = User.create(:username => "Dave", :email => "dave@dave.com")
+davepost = Post.create(:title => "First Post", :content => "Hi, I'm Dave.  This is my first post!")
+intro = Category.create(:name => "Intro")
+davepost.categories << intro
+

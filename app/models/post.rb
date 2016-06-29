7c5500183757c06a19073b9eb7ef7c92b073e030
brad r <brad72287@gmail.com>
2016-06-29 19:18:33 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories#, reject_if: proc { |attributes| attributes['title'].blank? }
+
+  def unique_users
+    arr = []
+    self.comments.each do |comment|
+      arr << comment.user unless arr.include?(comment.user)
+    end
+    arr
+  end
 
 end

274f1c7ec341ccef511e701f07056491d42aa70d
reichardn <reichardn@gmail.com>
2016-07-06 04:00:23 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    
+    comment = Comment.new(content: comment_params[:content], post_id: comment_params[:post_id])
+    if comment_params[:user_id] != ''
+      comment.user_id = comment_params[:user_id]
+    else
+      comment.user_attributes = comment_params[:user_attributes]
+    end
+    comment.save
     redirect_to comment.post
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def unique_users
+    self.users.uniq{|u| u.user_id}
+  end
 
 end

7785324a8b230b27555db27d3b5a84c9fc4c528d
tttra <tothutra@gmail.com>
2016-07-10 22:08:03 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def commented_posts
+    commented_posts = self.comments.collect do |c|
+      c.post
+    end
+    commented_posts.uniq
+  end
 end

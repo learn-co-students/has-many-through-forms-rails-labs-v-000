494fb012a34fba59bf9e98b47dea2d8dcff97bee
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-22 15:07:37 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,14 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(
+    :title,
+    :content,
+    category_ids:[],
+    categories_attributes: [:name])
   end
+
+#  def comment_params
+#    params.require(:comment).permit(:user_id, :content)
+#  end
 end

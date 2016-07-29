acc0bab3c8e73ee3dd8e89fd0537e3632b1a3dd6
ndaniele <n.daniele@gmail.com>
2016-07-29 13:18:02 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
     category_attributes.values.each do |category_attribute|
   #    if category_attributes != " "
         category = Category.find_or_create_by(category_attribute)
-        self.post_categories << category
+        self.post_categories.build(category: category)
    #   end
     end 
   end

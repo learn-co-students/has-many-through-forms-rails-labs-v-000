500d6e9606470ff8723e28b06a545b304d273f66
kaileighrose <kaileighrose@gmail.com>
2016-08-03 12:26:49 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
+require 'pry'
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to user
+  end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    binding.pry
+    params.require(:user).permit(:post_id, :user_id, :username, user_attributes: [:username])
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |username|
+      if !username.empty? && username != ""
+        user = User.find_or_create_by(username: username)
+        self.user = user
+      end
+    end
+  end
 end

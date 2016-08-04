c70d145c4b05fc051b60e6980ed21f5e48999f4e
sarond <saron.desta@gmail.com>
2016-08-03 20:12:34 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+3.times do
+   @post = Post.create(title: Faker::Book.title, content: Faker::Hipster.paragraph)
+   @post.categories.build(name: Faker::Pokemon.name)
+   @user = User.create(username: Faker::Internet.user_name)
+   @comment = @post.comments.build(content: Faker::Lorem.sentence, user: @user)
+   @post.save
+ end

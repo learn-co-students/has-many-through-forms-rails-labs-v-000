c24a202b93380cc8aee64674640867a1ddcba176
arturusfury <bri.reynolds@gmail.com>
2016-08-11 15:28:13 -0500

    Done.

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'categories', type: 'feature' do
-
   before do
     @post = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
     @cool = @post.categories.build(name: "Cool")
@@ -9,14 +8,9 @@ describe 'categories', type: 'feature' do
   end
 
   describe 'show page' do
-
     it 'should display all of the related posts' do
       visit category_path(@cool)
       expect(page).to have_link(@post.title, href: post_path(@post))
     end
-
   end
-
-
-
 end

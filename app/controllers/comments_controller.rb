940e242c90e34e0160b0a1e852edd82b292974f3
CodeByLine <yleventhal@ymail.com>
2016-08-11 14:26:35 -0400

    2 errors left-in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,22 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    redirect_to comment.post
+    comment.save
+    redirect_to posts_path(@post.comment)
   end
 
+  # def create
+  #   comment = Comment.create(comment_params)
+  #     comment.save if comment.user.unique?
+  #     redirect_to comment.post
+  #   else
+  #     "You cannot comment more than once"
+  #   end
+  # end
+  #
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, :user_attributes:[:username])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,13 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
   private
 
   def post_params

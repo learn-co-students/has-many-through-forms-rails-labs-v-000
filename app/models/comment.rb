799547fb4eaa8e8ad96e113478fcebb7a8be5555
Joseph Lim <joseph.d.lim@gmail.com>
2016-08-12 13:39:44 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def user_attributes=(user_attributes)
+      user = User.find_or_create_by(user_attributes)
+      self.user = user
+  end
 
 end

5bf682498135581191c1d79619751359a1e35406
CodeByLine <yleventhal@ymail.com>
2016-08-11 21:59:52 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,20 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    comment = Comment.new(comment_params)
+    if comment_params[:user_id] != ''
+      comment.user_id = comment_params[:user_id]
+    else
+    comment.user_attributes = comment_params[:user_attributes]
+    end
     comment.save
-    redirect_to posts_path(@post.comment)
+    redirect_to comment.post
   end
 
-  # def create
-  #   comment = Comment.create(comment_params)
-  #     comment.save if comment.user.unique?
-  #     redirect_to comment.post
-  #   else
-  #     "You cannot comment more than once"
-  #   end
-  # end
-  #
+
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, :user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end

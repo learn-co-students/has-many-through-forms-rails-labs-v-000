491d40264435ddfd6ed22ba59b534cf6fa9df3a7
mcclains2003 <mcclains2003@gmail.com>
2016-08-12 10:17:38 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, :reject_if => lambda { |a| a[:name].blank? }
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  end
+
 
 end

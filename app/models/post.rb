54447ca8b6bf551ec137e95ca300a760976a428d
akizzles <aykim002@gmail.com>
2016-08-14 17:56:23 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(cat_attrs)
+    cat_attrs.values.each do |cat_attr|
+      cat = Category.find_or_create_by(cat_attr)
+      self.categories << cat
+    end
+  end
 
+  
 end

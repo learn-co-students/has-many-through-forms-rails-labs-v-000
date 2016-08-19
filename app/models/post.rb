3a9c7c4fbd1320b3d42da38b66adb37b8934650a
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-19 11:05:43 -0400

    still form question

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(attributes_hash)
+    attributes_hash.values.each do |val|
+      category = category.find_or_create_by(val)
+      self.categories << category
+    end
+  end
 
 end

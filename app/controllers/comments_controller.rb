8391705f351b574859faa012df6d2679ce68aacb
Michael Chen <michael.chen.1120@gmail.com>
2016-08-23 05:19:39 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    @comment = Comment.create(comment_params)
+    redirect_to comment_path(@comment)
+  end
+
+  def show
+    @comment= Comment.find(params[:id])
   end
 
   private

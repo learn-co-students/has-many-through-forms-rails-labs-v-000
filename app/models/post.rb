26472b23b5fb9612a3b17a541aa83af2fe2da3bc
steveafrost <ffrostt@gmail.com>
2016-08-26 00:17:29 -0400

    first attempt

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :users
 
+  def users_attributes=(users_attributes)
+		user = User.find_or_create_by(params[:post][:users_attributes]["0"][:username])
+		self.users << user
+	end
 end

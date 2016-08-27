dba3394a7c3e9f56a01335747de596e4f64da768
steveafrost <ffrostt@gmail.com>
2016-08-26 21:40:21 -0400

    completed using has many through form with nested attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  
+  accepts_nested_attributes_for :categories
 
-  accepts_nested_attributes_for :users
-
-  def users_attributes=(users_attributes)
-		user = User.find_or_create_by(params[:post][:users_attributes]["0"][:username])
-		self.users << user
-	end
 end

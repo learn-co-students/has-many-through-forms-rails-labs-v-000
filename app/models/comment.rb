6ca89b3d3b93af423d8bc89bf880704e56a62ad4
Jesse Novotny <jessenovotny@gmail.com>
2016-08-26 17:46:15 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    user_attributes.each do |attribute, value|
+      self.user = User.find_or_create_by(attribute.to_sym => value) if !value.empty?
+    end
+  end
+
+  def user_id=(user_id)
+    self.user = User.find(user_id)
+  end
+
+  def post_id=(post_id)
+    self.post = Post.find(post_id)
+  end
 end

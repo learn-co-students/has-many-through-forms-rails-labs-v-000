387069583e0dcbdc826c2ad26e46ad1e995311e7
bshap93 <bshap93@gmail.com>
2016-09-07 17:02:34 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :user, #reject_if? :new_user?
 
 
   def user_name

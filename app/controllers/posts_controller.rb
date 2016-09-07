89d71695eed9be65a232480d50691675105f8100
bshap93 <bshap93@gmail.com>
2016-09-07 16:53:04 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def show
+    @comment = Comment.new
     @post = Post.find(params[:id])
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,22 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+
+  def user_name
+    self.user.username
+  end
+
+  def new_user?
+    binding.pry
+  end
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.user = user
+    end
+  end
+
 end

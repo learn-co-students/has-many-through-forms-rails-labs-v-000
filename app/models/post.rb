c1c956a201cbb9d3eaab090f9e0037ebf6f95600
dannyenfp <daniel.valmas@gmail.com>
2016-09-14 13:20:03 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,17 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
 
+  def categories_attributes=(category_attributes)
+    #{"0"=>{"name"=>"sad"}}
+    # FIND or CREATE the category regardless of whether this post has it... 
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.post_categories.build(:category  => category)
+      
+      #this is inefficient because it'll return all categories 
+      #self.categories << category
+    end
+  end
+
 end
+ 

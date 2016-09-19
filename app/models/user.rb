fa8710f5ee85a51df442c3ac6aa3fdf50f5fac77
miclp <michaelpeimani@gmail.com>
2016-09-19 16:58:56 -0700

    Add user show page and check for unassociated comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def valid_comments
+    self.comments.select do |comment|
+      if comment.post_id != nil
+        comment
+      end
+    end
+  end
 end

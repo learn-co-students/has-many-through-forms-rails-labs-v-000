6e139f0944056669ffc93cc5541ab0c42b85dd54
miclp <michaelpeimani@gmail.com>
2016-09-19 16:40:09 -0700

    Fix fields_for in comment form to retain associations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    binding.pry
     comment = Comment.create(comment_params)
-    comment.post = Post.find(params[:post_id])
-    comment.save
     redirect_to comment.post
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user:[:username])
+     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end

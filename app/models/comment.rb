95e800fd891c4e56c5746c75727afea9f2d2b7dc
tylertaylor <vikingtyty@gmail.com>
2016-10-01 12:39:25 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,4 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: lambda { |x| x[:username].empty? }
 
-  def no_user
-    binding.pry
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,18 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :comments, reject_if: :reject_comments
-
+  accepts_nested_attributes_for :comments
+  
   def categories_attributes=(category_attributes)
     category_attributes.each do |i, category_attribute|
       category = Category.find_or_create_by(name: category_attribute[:name])
       # self.categories << category # doesn't work
       self.post_categories.build(:category => category)
     end
-  end
-
-  def reject_comments
-    binding.pry
   end
 
   def all_unique_commentors(post)

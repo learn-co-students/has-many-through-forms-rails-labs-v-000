83dbf0b6b386e4fcc4537c8b48a45a5320d37c9c
Traci7822 <Tracir@gmail.com>
2016-10-07 20:47:10 -0600

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    binding.pry
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :username_blank?
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
-  def username_blank?
-    self.user.username == "" || self.user.username.blank?
-  end
 end

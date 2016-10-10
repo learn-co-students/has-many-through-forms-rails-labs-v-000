d123b280fc508bf50387989d146d7a11f00dd7cd
Ivan Pilot <ivanpilot@gmail.com>
2016-10-10 04:09:39 +0100

    6 more tests to pass

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], user_ids:[], comments_attributes: [:content])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,28 +7,13 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
   accepts_nested_attributes_for :comments
 
-  # attr_reader :comment_content
-
   def categories_attributes=(category_attributes)
-    # binding.pry
     category_attributes.values.each do |cat_attr|
       category = Category.find_or_create_by(cat_attr)
       self.categories << category
     end
   end
 
-  def comments_attributes=(comments_attributes)
-    # binding.pry
-    comments_attributes.values.each do |cmt_attr|
-      comment = Comment.create(cmt_attr)
-      self.comments << comment
-    end
-  end
-
-  def user_ids=(user_ids)
-    
-    self.users << User.find(users)
-  end
 
 
 end

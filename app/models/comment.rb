3fff8e420989a76604ff67125e37aa96c623b7c3
William Jones <enquiries@forestfed.com>
2016-10-24 18:10:40 +0000

    has many through forms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
+
+  def category_attributes=(user)
+    self.user = User.find_or_create_by(username: user.username)
+    self.user.update(user)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
+
+  def category_attributes=(category)
+    self.category = Category.find_or_create_by(name: category.name)
+    self.category.update(category)
+  end
 
 end

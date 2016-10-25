7f4a1613b9ff33f23f7c88e84c087a95feee1fb5
smithWEBtek <brad@smithwebtek.com>
2016-10-25 19:29:48 -0400

    back to 1 remaining test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
-<<<<<<< HEAD
-
 
   def downcase_name
     name.downcase
   end
 
 end
-=======
- 
-end
->>>>>>> 98fcc365fe2cb3c23cb67cf68ceab9b7e6b0110c
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-<<<<<<< HEAD
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |i, category_attributes|
       if category_attributes[:name].present?
@@ -17,22 +16,5 @@ class Post < ActiveRecord::Base
         end
       end
     end
-  end
-
-  def new_comment_new_user
-    
-  end
-
-  def new_coment_existing_user
-
-=======
-  accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['username'].blank? }
- 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
-    end 
->>>>>>> 98fcc365fe2cb3c23cb67cf68ceab9b7e6b0110c
   end
 end

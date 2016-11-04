5ed0d7a731605000e7c0e16ac4faecd1569a0ce9
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-04 13:34:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  
+  def categories_attributes=(attributes)
+    attributes.each do |key, new_category|
+      if new_category[:name].present?
+        @category = Category.create(new_category)
+        self.post_categories.build(:category => @category)
+      end
+    end
+  end
 
 end

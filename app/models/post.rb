d1d959be6ddd5a2d755ce3654d1a2a95caee78b4
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-07 22:21:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,25 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  
-  def categories_attributes=(attributes)
-    attributes.each do |key, new_category|
-      if new_category[:name].present?
-        @category = Category.create(new_category)
+
+  def categories_attributes=(category_attributes)
+    category_attributes.each do |i, category|
+      if category[:name].present?
+        @category = Category.create(name: category[:name])
         self.post_categories.build(:category => @category)
       end
     end
   end
 
 end
+# {"title"=>"test title", 
+#   "content"=>"test content ", 
+#   "category_ids"=>["1", "2", ""], 
+#   "categories_attributes"=>{"0"=>{"name"=>"category 3 - brand new"}}},
+#  "commit"=>"Create Post", "controller"=>"posts", "action"=>"create"}
+
+
+#  1. need to be able to take these parameters and build a new object
+#  2. if cateogire attributes are there, you create a new category out of them
+
+

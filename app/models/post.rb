f2d0e409e7a8b23dd220b4bb16107697c5f734ee
AliceKB <alicekristinabalbuena@gmail.com>
2016-11-13 03:27:23 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,22 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :comments
 
+  def categories_attributes=(category_hashes)
+    category_hashes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
+
+  def unique_users
+    self.comments.collect do |com|
+      com.user
+    end.uniq
+  end
 end

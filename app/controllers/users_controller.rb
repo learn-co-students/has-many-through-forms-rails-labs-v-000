20245cebfa8e19525657dba0e3eb9adde001e6f9
Bob Habig <rfhab2@gmail.com>
2016-11-23 00:36:52 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
-  def user_attributes=(user_attributes)
-    binding.pry
-  end
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 end

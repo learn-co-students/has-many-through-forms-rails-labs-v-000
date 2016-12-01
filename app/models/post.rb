5bd02032a1c0e2f55923df471eb115c2ff0a4db4
kkterai <kkterai@gmail.com>
2016-12-01 00:04:57 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :comments,:allow_destroy => :true
+  accepts_nested_attributes_for :users
 
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+        category= Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
+
+  def comments_attributes=(comments_hashes)
+
+  end
 end

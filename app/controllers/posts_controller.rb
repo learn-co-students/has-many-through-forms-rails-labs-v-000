71991a59af8af046c3d89124ec34363727b4dace
kkterai <kkterai@gmail.com>
2016-12-06 20:47:34 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    raise params.inspect
     @post = Post.find(params[:id])
     @post.update(post_params)
 
@@ -27,6 +28,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment_ids:[], comments_attributes: [:content, :post_id, :user_id]
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment_ids:[], comments_attributes: [:content, :post_id, :user_id])
   end
 end

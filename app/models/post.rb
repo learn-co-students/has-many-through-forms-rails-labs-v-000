2a5722cfecf4f30a1df449efa46c3abc9e392b53
kkterai <kkterai@gmail.com>
2016-12-06 21:04:07 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   accepts_nested_attributes_for :comments
-
+  
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |i, category_attributes|
       if category_attributes[:name].present?

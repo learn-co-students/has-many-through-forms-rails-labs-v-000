2f6dc84af46c2cc89ad39bad4f67d0bb358528b6
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-01-08 19:13:33 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,11 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
 
+  def categories_attributes=(attr_hash)
+    attr_hash.values.each do |attr|
+      category = Category.find_or_create_by(attr)
+      self.categories << category
+    end
+  end
+
 end

d22d5b6abd703ae463bc8124a8031a0db896ecdb
Ray Mulligan <rmullig2@optonline.net>
2017-01-10 20:43:29 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def show
+    @comment = Comment.new
     @post = Post.find(params[:id])
   end
 
@@ -12,6 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    #binding.pry
     post = Post.create(post_params)
     redirect_to post
   end
@@ -19,6 +21,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comments_attributes: [:content])
   end
 end

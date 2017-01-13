dded2537258821d5228eb1d3193e721cae12864a
Bill <storywr@gmail.com>
2017-01-13 15:28:56 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,8 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.create(comment_params)
-    redirect_to @comment.post
+    comment = Comment.create(comment_params)
+    redirect_to comment.post
   end
 
   private

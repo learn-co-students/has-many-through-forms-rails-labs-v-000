2e5a4d6614cfc82702aa6ec2e44f6db848646f8f
Scott Young <young.scottw@gmail.com>
2017-02-14 17:26:33 -0600

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    comment = Comment.new(comment_params)
+    if params[:comment][:user_attributes][:username] != ""
+      comment.user = User.create(:username => params[:comment][:user_attributes][:username])
+    end
+    comment.post_id = params[:post_id]
+    comment.save
     redirect_to comment.post
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+category1 = Category.create(name: "Cool")
+category2 = Category.create(name: "Neat")
+category3 = Category.create(name: "Awesome")
+
+post1 = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+post2 = Post.create(title: "Pretty cool", content: "The dude abides")
+post3 = Post.create(title: "I like to eat apples and bananas", content: "They are delicious")
+
+post1.categories << category1
+post1.categories << category2
+post2.categories << category2
+post2.categories << category3
+post3.categories << category1
+
+user1 = User.create(username: 'Scott')
+user2 = User.create(username: 'Layne')
+
+comment1 = Comment.create(content: "This was great!", user: user1, post: post1)
+comment2 = Comment.create(content: "This dude does not abide", user: user1, post: post2)
+comment3 = Comment.create(content: "Cool, cool, cool, cool", user: user2, post: post1)

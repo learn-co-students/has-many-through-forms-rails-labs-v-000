e659525ddce1fa1050f273eec2e49daf0caa0193
Tayari Appiah <cjhhh.ta@gmail.com>
2017-02-17 16:37:21 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
+    binding.pry
     @comment = Comment.create(comment_params)
     redirect_to post_path(@comment.post_id)
   end
@@ -8,6 +9,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username, :email])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end

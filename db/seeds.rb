e5ee75cd72266709742121a22abec529753c1072
Jon foster <redcabinny@gmail.com>
2017-02-23 23:28:27 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(:title => "First post", :content => "Yeah boi")
+Post.create(:title => "Second post", :content => "aww yeah")

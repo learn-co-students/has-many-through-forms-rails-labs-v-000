3ccdae845498392b6f47054453fc655a3b6178fa
M. Simon Borg <msimonborg@gmail.com>
2017-02-27 08:23:17 -0500

    passing comments feature tests and model tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,19 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def time
+    return unless created_at
+    "#{created_at.strftime("%B %-d, %Y")} at #{created_at.strftime("%k:%M")}"
+  end
+
+  def username
+    try(:user).try(:username)
+  end
+
+  def user_attributes=(user_attributes)
+    return if user_attributes[:username].blank?
+    self.user = User.find_or_create_by(user_attributes)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def distinct_users
+    users.distinct
+  end
 end

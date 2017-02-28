5649a49aca7d8580deb959a7da3057eb50a35079
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-02-28 11:10:49 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  #accepts_nested_attributes_for :categories
 
+  def categories_attributes=(attrs)
+    attrs.values.each do |attr|
+      category = Category.find_or_create_by(name: attr[:name])
+      self.categories.concat(category)
+    end
+  end
 
 end

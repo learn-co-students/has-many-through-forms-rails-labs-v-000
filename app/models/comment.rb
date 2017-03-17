f66e8d3dec3bf65402d2e0dd4629ca47f44ee3af
Kevin Adkins <kevinladkins@gmail.com>
2017-03-17 18:01:52 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes(attributes)
+      raise attributes.inspect
+      if attributes[:username].present
+      user = User.find_by(username: attributes[:username])
+      self.user_id = user.id
+    end
+ end
 
 end

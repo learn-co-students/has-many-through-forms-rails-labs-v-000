2b9c9f2d7a54f36bc2dae57fb8b617a8ac2c16e0
github username pianistps <github email address pianistps@yahoo.com>
2017-03-23 14:32:47 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if comment = Comment.create(comment_params)
+      redirect_to comment.post
+    else
+      raise comment_params.inspect
+    end
   end
 
   private

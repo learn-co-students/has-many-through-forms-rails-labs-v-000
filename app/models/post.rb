493ca71e923724286df731cab9129d844c2a27d6
John Pfingst <jpfingst@outlook.com>
2017-03-27 17:54:52 +0000

    remove leftover code

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,21 +4,10 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each { |category_attribute|
         self.categories << Category.find_or_create_by(category_attribute) if category_attribute[:name].size > 0
-    }
-  end
-
-  def user_attributes=(user_attributes)
-    user_attributes.each { |user_attribute|
-      attr_name  = user_attribute[0]
-      attr_value = user_attribute[1]
-      if attr_name == "username" and attr_value.size > 0
-        self.user = User.find_or_create_by(username: attr_value)
-      end
     }
   end
 

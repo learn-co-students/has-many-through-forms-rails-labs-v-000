69fb111154ce680f7839a9f39fc45100ec2f78d1
Siobhan <siobhan.harley@gmail.com>
2017-04-08 22:20:17 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :reject_user
+
+
+  def reject_user(user_attributes)
+     user_attributes['username'].blank?
+   end
+
+  def user_attributes=(user_attributes)
+    new_user = User.find_or_create_by( user_attributes)
+    self.user = new_user
+end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+  validates :username, presence: true
+
+
 end

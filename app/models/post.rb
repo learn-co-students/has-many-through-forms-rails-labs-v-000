13ffca4aa8fe87ea1023d318ae76e4736d3f2e78
Christina L. Cole <christinalcole@gmail.com>
2017-04-16 23:49:18 -0400

    prevent saving a blank category tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  # accepts_nested_attributes_for :categories, reject_if: :all_blank?
+  #accepts_nested_attributes_for :categories, reject_if: :all_blank?
 
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories<< category
+      if category_attribute[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        categories<< category
+      end
     end
   end
 end

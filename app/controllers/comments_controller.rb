74a4a9f31be3aa22b14d76b1aab2ac0c03516150
bortch89 <bortch@gmail.com>
2017-04-28 19:12:55 +0200

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_attributes][:username] != ""
+      @comment = Comment.create(comment_params)
+      redirect_to post_path(@comment.post_id)
+    else
+      @comment = Comment.create(comment_params_2)
+      redirect_to post_path(@comment.post_id)
+    end
   end
 
   private
@@ -10,4 +15,11 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
+  def comment_params_2
+    params.require(:comment).permit(:content, :post_id, :user_id)
+  end
+
+
+
 end

0504435e8e9d09fece2b2204157e8aad6139d670
Howard DeVennish <howarddevennish@gmail.com>
2017-04-29 14:49:09 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,14 @@ class Post < ActiveRecord::Base
 
   def uniq_users_list
     users = self.users.collect
-    users.map {|u| u }.uniq 
+    users.map {|u| u }.uniq
+  end
+
+  def categories_attributes=(attributes_hash)
+    binding.pry
+    category = Category.find_or_create_by(name: attributes_hash[:name])
+    self.post_categories.build(category: category)
+    self.save
   end
 
 end

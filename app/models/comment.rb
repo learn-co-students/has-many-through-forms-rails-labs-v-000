78a64ce11b1c5c8add2030f560e93a582fe8af88
Seth <itz.saga@gmail.com>
2017-04-30 14:34:50 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(attributes)
+    user = User.find_or_create_by(attributes)
+    self.user = user if self.user == nil
+  end
+
 end

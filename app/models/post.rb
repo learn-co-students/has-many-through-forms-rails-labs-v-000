6f5bba85128d4a2be490eb17a3183e4814c2bf7c
kkashuda <kkashuda@gmail.com>
2017-04-30 00:52:29 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,23 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  has_many :commentors, through: :comments, source: :user
+  belongs_to :user 
+
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+ def comments_attributes= (comments_attributes)
+    comments_attributes.values.each do |comments_attribute|
+        comment = Comment.create(comments_attribute)
+        self.comments << comment
+    end        
+end
 
 
 end

e7a21aeae31e72bf867fd2084ba0f4499a49122a
Howard DeVennish <howarddevennish@gmail.com>
2017-05-01 17:08:27 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,6 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  # accepts_nested_attributes_for :categories
-
-  def uniq_users_list
-    users = self.users.collect
-    users.map {|u| u }.uniq
-  end
 
   def categories_attributes=(attributes_hashes)
     attributes_hashes.each do |index_key, attribute_hash|

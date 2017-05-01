2f05637fb930bd3dce3a1cabe29fceb029385f7f
Kelsey Huse <kelseyhuse30@gmail.com>
2017-05-01 13:33:59 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,19 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  validates :content, presence: true
+
+  def username
+  	try(:user).try(:username)
+  end
+
+  def user_attributes=(user_attributes)
+    if user_attributes[:username].present?
+      @user = User.create(user_attributes)
+      self.user = @user
+     end
+  end
+  
 end

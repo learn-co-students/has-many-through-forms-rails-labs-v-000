febf853d0289f86840023ba847e29b44259c20f0
mxdavis <md01@me.com>
2017-05-10 13:08:39 +0300

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+
+  def user_attributes=(user_attributes)
+    if user_attributes[:username].present?
+      self.build_user(user_attributes)
+    end
+  end
+
 end

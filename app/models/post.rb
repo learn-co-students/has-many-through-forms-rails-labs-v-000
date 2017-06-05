082c99bc7218d47215d6f126280d47535750a11c
vbox <sattelbergerp@gmail.com>
2017-06-04 20:11:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
-  has_many :users, through: :comments
+  has_many :users, -> { distinct }, through: :comments
 
+  accepts_nested_attributes_for :categories
 
 end

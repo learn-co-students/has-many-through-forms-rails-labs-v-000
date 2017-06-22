03db6fcc7f5738a9a7384599a9b89e75c60ef82d
mmelmer <mikemelmer@gmail.com>
2017-06-21 22:55:32 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+  
+  # def new
+  #   @user = User.new
+  # end
+
+  # def create
+  #   user = User.create(user_params)
+  #   redirect_to user
+  # end
+
   def show
     @user = User.find(params[:id])
   end

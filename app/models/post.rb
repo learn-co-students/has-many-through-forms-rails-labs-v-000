8e3db03cbdf9b8708e812a0e98b21fc70629b0a8
Aditi Green <asaki444@gmail.com>
2017-07-04 14:41:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments 
 
-
+  def commments_attributes=(commments_attributes)
+    commments_attributes.values
+  end
 end

8e5a6e542478bd1c3bfb0ed6f3412ebd7e69bcc0
Allison Chesky <afc340@nyu.edu>
2017-07-12 12:26:39 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    if comment_params[:user_attributes][:username] == ""
+      user = User.find_by_id(comment_params[:user_id])
+      post = Post.find_by_id(comment_params[:post_id])
+      comment = Comment.create(content: comment_params[:content], post: post, user: user)
+    else
+      post = Post.find_by_id(comment_params[:post_id])
+      comment = Comment.create(content: comment_params[:content], post: post)
+      comment.user_attributes=(comment_params[:user_attributes])
+    end
     redirect_to comment.post
   end
 
@@ -10,4 +18,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
 end

521ebb065ad84b5920639b2b8f807c8f4331c063
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-26 18:26:20 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to post_path(@post)
+    post = Post.new(post_params)
+    if post.save
+      redirect_to post_path(post)
     else
       render :new
     end

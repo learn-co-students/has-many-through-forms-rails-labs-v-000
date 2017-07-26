40ba7995e6378904171fb6f38822b53b004458de
jinstrider2000 <efrainperezjr@live.com>
2017-07-26 11:11:17 -0400

    more progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+  
+  def find_comments_by_post(post)
+    self.comments.where(post_id: post.id)
+  end
 end

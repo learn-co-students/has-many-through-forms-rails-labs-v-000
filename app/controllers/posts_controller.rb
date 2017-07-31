7073fe2a8dd908afad076b798205ee43b89585fd
jessica fletcher <cara.kane@gmail.com>
2017-07-31 20:32:04 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,25 +9,16 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    2.times @post.comments.build
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @comment = Comment.create(params[:comments_attributes])
-    @post.comments << @comment
+    @post = Post.create(post_params)
     redirect_to @post
   end
-
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, comment_ids:[], comments_attributes: [:user, :content], category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|

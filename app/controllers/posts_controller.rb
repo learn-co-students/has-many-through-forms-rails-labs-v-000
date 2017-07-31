63627587ebca9e32101bee6a69f588ca3152e385
jessica fletcher <cara.kane@gmail.com>
2017-07-31 17:16:46 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    2.times @post.comments.build
   end
 
   def create
@@ -16,9 +17,17 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @comment = Comment.create(params[:comments_attributes])
+    @post.comments << @comment
+    redirect_to @post
+  end
+
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, comment_ids:[], comments_attributes: [:user, :content], category_ids:[], categories_attributes: [:name])
   end
 end

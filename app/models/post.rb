a48978341ac1860552d74eb84320267c1284f55c
Susanna Klosko <sdklos@gmail.com>
2017-08-04 05:46:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].empty? }
+  accepts_nested_attributes_for :users, reject_if: proc { |attributes| not_unique?(attributes['username']) }
+  accepts_nested_attributes_for :comments
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def categories=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
 end

1fd343010a5a2eed443b176e224b70ec8fabed86
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-06 13:40:02 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def post_title
+    self.post.title
+  end
+
+  def user_name
+    self.user.username
+  end
+
 end

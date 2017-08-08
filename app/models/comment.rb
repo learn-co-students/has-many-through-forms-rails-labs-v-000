2d0fe4e09b1c562017c63060db4f1bf18c822a4d
cs-j <cj@mail.co.de>
2017-08-08 16:01:08 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def user
+    User.find_by_id(self.user_id)
+  end
+
+  def username
+    self.user.username
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  validates :title, presence: true
+  validates :content, presence: true
 
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].empty? }
+  accepts_nested_attributes_for :users, reject_if: proc { |attributes| not_unique?(attributes['username']) }
+  accepts_nested_attributes_for :comments
 
+  def categories=(category_attributes)
+    category_attributes.values.each do |attribute|
+      category = Category.find_or_create_by(attribute)
+      self.categories << category
+    end
+  end
 end

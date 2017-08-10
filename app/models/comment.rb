d035b23f9b55985d222665a6a8a6ccbac02e3d02
blinded93 <blinded93@gmail.com>
2017-08-09 17:28:44 -0700

    passed tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    if user = User.find_by(username: user_attributes[:username])
+      self.user = user
+    elsif user_attributes[:username].present?
+      self.build_user(user_attributes)
+    end
+  end
 end

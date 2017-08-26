9676352cedba5fe59707b409e5c0abaf20162deb
skmcloughlin <skmcloughlin@gmail.com>
2017-08-26 16:13:43 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    self.user = User.where(id: user_id).first_or_create do |user|
+      user.username = user_attributes[:username]
+    end
+  end
 end

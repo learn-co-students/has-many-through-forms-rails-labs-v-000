6d4d72477cd27293af843350137d309e86c7ef35
gb23 <gregbenj@hotmail.com>
2017-09-01 15:58:07 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+      if !user_attributes.has_value?("")
+        user = User.find_or_create_by(user_attributes)
+        self.user = user
+        self.save
+      end
+  end
 end

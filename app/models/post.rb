47f1e6359f91633f421651aa4881c237bf44ee95
Terry Blue <terryblue@comcast.net>
2017-09-09 20:25:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,14 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      # or: category_attributes.each do |i, category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(:category => category)
-      # or: self.categories << category
+    # or: category_attributes.each do |i, category_attribute|
+      if category_attribute[:name].present? # Don't create a blank category
+        category = Category.find_or_create_by(category_attribute)
+        if !self.categories.include?(category) # only create the post category if the post doesn't have it
+          self.post_categories.build(:category => category)
+          # or: self.categories << category
+        end
+      end
     end
   end
 

d8d3e455fadb08b98e9fe488998c504bbb751a65
Terry Blue <terryblue@comcast.net>
2017-09-09 16:45:58 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      # or: category_attributes.each do |i, category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.post_categories.build(:category => category)
+      # or: self.categories << category
+    end
+  end
 
 end

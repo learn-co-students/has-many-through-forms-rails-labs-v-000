02a645c6d4a81827ecccf70419a61b76b5c338b1
Terry Blue <terryblue@comcast.net>
2017-09-09 21:05:36 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
 
+  def new
+    binding.pry
+    @comment = Comment.new
+  end
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post

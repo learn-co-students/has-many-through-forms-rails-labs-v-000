c676fe94c35fcbbb577fa7e99197bcdf9fab8f55
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-09-17 08:26:22 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def user_attributes(user_attributes)
+    user_attributes.each do |user_attribute|
+      self.users.build(user_attribute)
+    end
+  end
 end

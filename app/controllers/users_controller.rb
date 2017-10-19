e97545918164f63b21d291450beeac0211593dac
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2017-10-19 13:05:43 -0500

    attempts on comments creates a comment with an existing user spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,14 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
+    #raise @user.inspect
+    #raise @posts.inspect
   end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, comment_ids:[], comments_attributes:[:comment]))
+    params.require(:user).permit(:username, :email, comment_ids:[], comments_attributes:[:comment])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
-  has_many :comments
-  has_many :posts, through: :comments
+  has_many :comments#add methods such as ??
+   #http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#Nested Associations
+  # User.comments.empty?
+  # User#comments.empty?, 
+  # User#comments.size, 
+  # User#comments, 
+  # User#comments<<(milestone), 
+  # User#comments.delete(milestone), 
+  # User#comments.destroy(milestone), 
+  # User#comments.find(milestone_id), 
+  # User#comments.build, 
+  # User#comments.create
+    has_many :posts, through: :comments#add methods such as ????
+   #https://ducktypelabs.com/how-a-has_many-through-association-works-in-practice/
+
+
   def comments_attributes=(comment_attributes)
    comment_attributes.values.each do |comment_attribute|
      comment = Comment.find_or_create_by(comment_attribute)

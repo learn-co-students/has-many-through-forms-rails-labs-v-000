6aa41fbb3ef4d385b71fce884e75bbbfa90c42b3
laramontana <larabrightside@gmail.com>
2017-10-21 11:45:27 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |categories_attribute|
+      self.categories << Category.find_or_create_by(categories_attribute) if !categories_attribute.values.empty?
+    end
+  end
+
 end

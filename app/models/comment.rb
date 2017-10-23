2f68553ab8c3932f2df2443ec78a95a33e182b7a
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-22 23:44:00 -0700

    Done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
+        user = User.new(user_attributes)
+        self.user = user
+        user.save
+    end
+  end
 end

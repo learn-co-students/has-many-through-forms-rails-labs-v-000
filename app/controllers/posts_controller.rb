2813af4e60b3d42a4c05ff82a456fcf9abc21509
Alexandra Wright <superbiscuit@gmail.com>
2017-10-23 18:08:38 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
+   
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.build_user
   end
 
   def index
@@ -12,6 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
+
     post = Post.create(post_params)
     redirect_to post
   end

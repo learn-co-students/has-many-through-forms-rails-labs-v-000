91d886dc41a72bd04541de6151d8409d512ce62e
Donna <donnamcla@gmail.com>
2017-10-27 00:13:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,15 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments
+  # accepts_nested_attributes_for :comments
 
-  # def categories_attributes=(category_attributes)
-  #   category_attributes.values.each do |category_attribute|
-  #     category = Category.find_or_create_by(category_attribute)
-  #     self.categories << category
-  #   end
-  # end
+  def categories_attributes=(category_hashes)
+    category_hashes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      # self.categories << category
+      # that's inefficient, because it will return all categories, and that could be a lot, so instead, this is more surgical.  does only what it needs to do:
+      self.post_categories.build(:category => category)
+    end
+  end
 
 end

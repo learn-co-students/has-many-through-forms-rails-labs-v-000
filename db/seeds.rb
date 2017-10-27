08705915c9928be39ee7ea0379d89510eb6fd988
aisayo <aisayo@icloud.com>
2017-10-27 20:33:27 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create([{username: "aysan", email: "aosayo" }])
+comment = Comment.create(content: "This is great", post_id: 1, user_id: 1)

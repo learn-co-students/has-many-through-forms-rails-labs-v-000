3148b9a5fe1815631674ec486e24f68daee0126f
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-10-31 09:37:01 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(username: "rjcolmed", email: "rjcolmed@email.com")
+user_2 = User.create(username: "caro", email: "caro@email.com")
+post = Post.create(title: "This is a post", content: "Blah blah blah")
+post_2 = Post.create(title: "This is another post", content: "Blor blor blor")
+
+category = Category.create(name: "wow")
+
+post.categories << category
+post_2.categories << category
+
+comment = Comment.create(content: "yayayayayayay", user: user, post: post)
+

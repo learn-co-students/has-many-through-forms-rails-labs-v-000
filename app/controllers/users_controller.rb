c69310a055088e0381107f644c10855b497581c5
Cynthia Rittenbach <crittenbach@gmail.com>
2017-11-01 21:16:09 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = []
+    Comment.all.each do |c|
+      if c.user_id = @user.id
+        @posts << Post.find(c.post_id)
+      end
+    end
   end
 
 end

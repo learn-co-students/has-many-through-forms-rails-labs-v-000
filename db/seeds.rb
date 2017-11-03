6e1574a1c81b384f24aa71469f58d5274755c40d
jamesonbass1987 <jamesonbass@gmail.com>
2017-11-03 15:45:37 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Category.create(name: "Cool")
+Category.create(name: "Awesome")
+Category.create(name: "Funny")
+
+Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+Post.create(title: "So Awesome", content: "This is such a cool post")
+
+
+User.create(username: 'RealBigFish')
+User.create(username: 'MeganBass')
+User.create(username: 'LilyDog')

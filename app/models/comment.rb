1724880bb615d4e90e967c082332e1140546e962
lpassamano <leighayna.josele@gmail.com>
2017-11-05 17:45:28 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user
-
-  def user_attributes=(attributes)
-    user = User.find_or_create_by(attributes)
-    self.user = user
-  end
+  accepts_nested_attributes_for :user, :reject_if => :all_blank
 end

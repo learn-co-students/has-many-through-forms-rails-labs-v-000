a76aaee4cce76fccf12ba081f0a4c8b18e738f7b
Sophia H <shhubeny@gmail.com>
2017-11-08 20:58:56 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :username_blank
+
+  def username_blank(user_attributes)
+    user_attributes['username'] == ""
+  end
+
 end

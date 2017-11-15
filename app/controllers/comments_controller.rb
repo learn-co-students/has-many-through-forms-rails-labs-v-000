ddc3a2c56ef8256a29f9cb1d74871f854440a921
Christine Tran <christinetran825@gmail.com>
2017-11-15 06:40:02 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,11 @@ class CommentsController < ApplicationController
     redirect_to comment.post
   end
 
+
   private
 
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
 end

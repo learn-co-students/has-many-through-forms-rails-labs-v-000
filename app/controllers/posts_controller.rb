03d5e12d8b41d4ae7b4c6d3328bf2e4bd79e553a
alberto <github email address>
2017-11-20 21:13:45 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.build_user
   end
 
   def index
@@ -9,6 +11,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  #accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  def username
+    self.user.username
+  end
+
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].blank?
+      self.build_user(user_attributes)
+    end
+  end
 
 end

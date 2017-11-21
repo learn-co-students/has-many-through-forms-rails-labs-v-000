8b17026e768efd5bac01d6dce0691c07ce1ba80d
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-11-21 06:29:14 +0000

    night

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # when post is shown, user can add comment
+  accepts_nested_attributes_for :users, reject_if: proc {|attr| attr.any?{|a| a.blank?}}
+  accepts_nested_attributes_for :categories, reject_if: proc {|attr| attr.any?{|a| a.blank?}}
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def users_attributes=(users_attributes)
+    users_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.users << user #apparently doesn't overwrite, because modified by active record
+    end
+  end
 end

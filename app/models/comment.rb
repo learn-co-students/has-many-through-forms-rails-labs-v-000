2d84a9f437991f13343dc72a7c154c1e60eb37a3
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-22 21:55:20 -0500

    done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :blank_user
+  accepts_nested_attributes_for :user, reject_if: :blank_username
 
   def user_name
     self.user.username
@@ -15,16 +15,16 @@ class Comment < ActiveRecord::Base
     self.content = comment_content
   end
 
-  def user_attributes=(user_attributes)
-    self.build_user(user_attributes)
-  end
+  # def user_attributes=(user_attributes)
+  #   self.build_user(user_attributes)
+  # end
 
   def user_attributes
     self.user
   end
 
-  def blank_user(user_attributes)
-    user_attributes[:username].empty
+  def blank_username(user_attributes)
+    user_attributes[:username].blank?
   end
 
 end

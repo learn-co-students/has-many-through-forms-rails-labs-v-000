84a996ccb3ef402270c723e8307cc0dd64dd5fb7
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-24 21:57:41 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.destroy_all
+Post.destroy_all
+User.destroy_all
+
+
+
+@category = Category.create(name: "Cool")
+@post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+@user = User.create(username: 'RealBigFish')
+
+@post = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+@cool = @post.categories.build(name: "Cool")
+@post.save

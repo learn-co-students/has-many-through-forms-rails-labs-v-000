25dda24be1f667282e2cb8a2184e8f433aab6343
github username <github email address>
2017-11-27 21:39:29 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,23 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_attributes][:username].empty?
+      @comment = Comment.create(comment_params_2)
+    else
+      @comment = Comment.create(comment_params_1)
+    end
+
+    redirect_to post_path(@comment.post)
   end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
+    def comment_params_1
+      params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    end
+
+    def comment_params_2
+      params.require(:comment).permit(:content, :post_id, :user_id)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  # macro on line 7 adds the #categories_attributes= method called on a post instance
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end

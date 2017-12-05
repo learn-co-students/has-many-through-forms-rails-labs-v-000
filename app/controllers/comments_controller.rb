2b57ce3ee500a28bc54f8fa6f7b4245c929b873c
emorgan05 <emorgan05@gmail.com>
2017-12-05 14:42:22 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    if comment_params["user_id"] != ""
+        user = User.find(comment_params["user_id"])
+        comment = Comment.create(comment_params)
+        comment.user = user
+        comment.save
+      else
+
+        comment = Comment.create(comment_params)
+      end
     redirect_to comment.post
   end
 

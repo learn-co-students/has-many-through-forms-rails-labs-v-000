021b4fd5d10b58e4632de5766c8482101818a14e
Ben <baschenbrenner@hotmail.com>
2017-12-10 20:19:57 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    
+    if comment_params[:user_attributes][:username] == ""
+       new_hash= comment_params.delete_if {|key,value| key == "user_attributes"}
+       comment = Comment.create(new_hash)
+       redirect_to comment.post
+    else
+      comment = Comment.create(comment_params)
+      redirect_to comment.post
+    end
   end
 
   private
 
   def comment_params
+    
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  accepts_nested_attributes_for :user
 
+
+  def username
+    self.user.username
+  end
+  
+  
 end

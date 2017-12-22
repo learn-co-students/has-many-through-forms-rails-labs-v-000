8cf5beaaaf6ddffa44882256110a9bf7676ff89a
Oski <github email address>
2017-12-22 10:07:20 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category)
+    category.each do |k, category_name|
+      @category = Category.find_or_create_by(category_name)
+      self.categories << @category
+    end
+  end
+
+  def user_attributes(users)
+    binding.pry
+  end
+
 
 end

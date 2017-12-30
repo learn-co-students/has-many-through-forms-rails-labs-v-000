4981e828e9dce61a0524ecac1a4928b31249ee34
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-30 03:11:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   
   accepts_nested_attributes_for :categories
+  # accepts_nested_attributes_for :commentslea
+  # accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
@@ -12,5 +14,6 @@ class Post < ActiveRecord::Base
       self.categories << category
     end
   end
+
 
 end

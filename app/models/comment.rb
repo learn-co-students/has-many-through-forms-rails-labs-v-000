876c5a5248e5c3677ca52c8323233f4800314e90
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-01 16:28:33 -0500

    All tests passing but 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  # accepts_nested_attributes_for :categories
+  #
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     self.categories << category
+  #   end
+  # end
+
 end

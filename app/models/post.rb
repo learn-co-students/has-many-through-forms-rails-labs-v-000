36f1185654ab6bf59c5d6dad0b08fc7f4288a497
Scott Wakeman <github email address>
2018-01-17 12:00:40 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
 
+      #do NOT create a category if it does NOT name
+      if category_attributes[:name].present?
+      #But do NOT add a category to a post if the category exists
+      #Check if this post already has this category already?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
 end

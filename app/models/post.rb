0d8950025e06d4b6d203c83bab85635a3e6ce555
Eric McKinney <mckinney99@gmail.com>
2018-01-21 19:35:08 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,14 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
   accepts_nested_attributes_for :users
 
+
   def categories_attributes=(category_attribute)
-    category_attribute.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+    category_attribute.each do |i, category_attributes|
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        self.post_categories.build(category: category)
+
+
+  
     end
   end
 

1d4eeb365197d86c0c15f2b0404d110719a64b26
Olena Ageyeva <eageeva@hotmail.com>
2018-01-24 21:42:24 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
 
   def user_attributes(user_hash)
     binding.pry
-   if user_hash[:username].present?
+  if user_hash[:username].present?
       user=User.find_or_create_by(username: user_hash[:username])
     end
   end

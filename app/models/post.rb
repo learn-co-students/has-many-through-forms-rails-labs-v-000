4446006eb85ace66043183ede7041882c4f5b243
miriam <miriam5000@gmail.com>
2018-01-24 12:33:56 -0500

    first commit, categories working

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
+  validates :name,  uniqueness: true
 
+  def categories_attributes=(categories_hashes)
+  	# {"0"=>{"name"=>"new cat"}, "1"=>{"name"=>"new cat1"}}
+  	# raise categories_hashes.inspect
+  	categories_hashes.each do |i, category_attributes|
 
+  		category = Category.find_or_create_by(name: category_attributes[:name])
+  	# self.categories.build --> not right -- 
+  	# self.categories <<category # but this is inefficient -- it will return all categories 
+  	# ??What table gets a new row when we associate a post and a category -- > post_categoru!! 
+  	# thus: 
+  		self.post_categories.build(:category => category)
+  	# instantaiate instance of the join model-- and pass in the category. 
+    	end
+  	end
 end
---
+++ notes.md
@@ -0,0 +1,24 @@
+notes.md
+
+1 posts have_many categories -- categories are unique. 
+categories have_many posts 
+
+post_categories
+
+has_many through 
+
+2. post nested form with smart non-duplicate categories 
+
+
+	--> the build the form 
+
+
+4. comment belongs-to a user 
+3. Posts have many comments 
+5. On show page  dropdown  for users 
+
+
+So much stuff in this lab. 
+6. validations. 
+
+post has many comments

6cca537c596dab91309a1b20bf3b0357801a2455
Justin Acker <justin@justinacker.com>
2018-02-02 19:50:06 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,11 +5,11 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :all_blank
 
-  def user_attributes=(user_attributes)
-    binding.pry
-    puts "this called anyway"
-    user = User.find_or_create_by(user_attributes)
-    self.user = user
-  end
+  # def user_attributes=(user_attributes)
+  # #  binding.pry
+  #   puts "this called anyway"
+  #   user = User.find_or_create_by(user_attributes)
+  #   self.user = user
+  # end
 
 end

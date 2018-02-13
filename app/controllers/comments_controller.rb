a136a1fc9d2df0da8d1c369e9b84f1301a817c99
github username <github email address>
2018-02-13 14:31:11 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,16 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    # binding.pry
     redirect_to comment.post
   end
+
+  # def update
+  #   comment = Comment.find_or_create_by(params[:id])
+  #   comment.update(content: comment_params[:content], user_id: comment_params[:user_id])
+  #   binding.pry
+  #   redirect_to comment.post
+  # end
 
   private
 

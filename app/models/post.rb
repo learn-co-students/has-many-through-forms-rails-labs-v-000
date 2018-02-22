df403bc8068e3f27fb829794b217dc4e3a5b1b70
PJ Frias <peejfrias@gmail.com>
2018-02-22 18:20:56 +0000

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,10 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories, reject_if: ""
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
   def category_ids=(ids)
-    # binding.pry
+    ids.delete("")
     ids.each do |id|
       self.categories << Category.find(id)
     end

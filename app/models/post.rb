bc742e17f5dda848f1fffc52811f9b782de42d1a
Mel Rudick <melissarudick@MacBook-Air.local>
2018-02-22 08:10:12 -0600

    all but comments tests passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(hashes)
+    hashes.each do |i, cat_attributes|
+      category = Category.find_or_create_by(name: cat_attributes[:name])
+      self.post_categories.build(category: category)
+    end
+  end
+  
 
 end

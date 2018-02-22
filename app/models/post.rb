e78d3b987d5d50d824a29d51d37dd56c5094f6f7
PJ Frias <peejfrias@gmail.com>
2018-02-22 18:03:41 +0000

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, reject_if: ""
 
   def category_ids=(ids)
-
+    # binding.pry
+    ids.each do |id|
+      self.categories << Category.find(id)
+    end
   end
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      binding.pry
       self.categories << Category.find_or_create_by(category_attribute)
     end
   end

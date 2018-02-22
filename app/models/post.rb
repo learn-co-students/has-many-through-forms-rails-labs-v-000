62022e96c7e27a489b9e1bde828915e6b8beed3a
PJ Frias <peejfrias@gmail.com>
2018-02-22 17:05:13 +0000

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,20 @@
+require 'pry'
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def category_ids=(ids)
+
+  end
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      binding.pry
+      self.categories << Category.find_or_create_by(category_attribute)
+    end
+  end
 
 end

58235f87e1d21087b40bc44666edb0ea0835e946
github username <github email address>
2018-02-21 22:12:53 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,31 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, :users, :comments
 
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      # do not create category if it doesn't have a name
+      # if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        # if !self.categories.include?(category)
+          # inefficient
+          # self.categories << category
+          self.post_categories.build(:category => category)
+        # end
+      # end
+    end
+  end
+
+  def users_attributes=(users_attributes)
+    users_attributes.values.each do |user_attributes|
+      user = User.find_or_create_by(user_attributes)
+      self.users << user
+    end
+  end
+
+  def comments_attributes=(comments_attributes)
+    self.comments.build(comments_attributes)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@category = Category.create(name: "Cool Stuff")
+@post = Post.create(title: "Feeling It", content: "I'm feeling it")
+@post.categories << @category
+@user = User.create(username: 'Marina')
+@comment = @post.comments.build(content: "Alright Post!", user: @user)
+@post.save
+
+@category = Category.create(name: "Class")
+@post = Post.create(title: "So Amazing", content: "This post is the post!")
+@post.categories << @category
+@user = User.create(username: 'Flat')
+@comment1 = Comment.create(content: "This was great!", user: @user, post: @post)
+@post.save
+
+@category = Category.create(name: "Wow")
+@post = Post.create(title: "Wowowow", content: "All about dogs")
+@user = User.create(username: 'Dogg')
+@post.categories << @category
+@user = User.create(username: 'Botya')
+@comment1 = Comment.create(content: "Completely agree!", user: @user, post: @post)
+@post.save

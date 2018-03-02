d536ea00c7d6ec0eb174c0f0a1bc751427e0c8dd
lizbur10 <liz.burton147@gmail.com>
2018-03-02 16:45:29 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :reject_user
+
+  def user_attributes=(user_attributes)
+    
+    if !reject_user(user_attributes)
+      user = User.find_or_create_by(user_attributes)
+      self.user = user
+    end
+  end
+
+  def reject_user(attributes)
+    attributes['username'].blank?
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :reject_categories
+
+  def categories_attributes=(categories_attributes)
+    # if !reject_categories(categories_attributes)
+      categories_attributes.values.each do |category_attribute|
+        category = Category.find_or_create_by(category_attribute)
+
+        self.post_categories.build(:category => category)
+        # could also do self.categories << category but this is more efficient
+      end
+    # end
+  end
+
+  def reject_categories(attributes)
+    attributes["0"]['name'].blank?
+  end
 
 end

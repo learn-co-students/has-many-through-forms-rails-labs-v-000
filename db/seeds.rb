4b506ff99b353b6826f1f2e1acbee1221528f942
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-04 13:41:46 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: "coolio")
+User.create(username: "denis", email: "denis@gmail.com")

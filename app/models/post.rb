37cb2a065200499b7937e489c259905dc9f56647
Jordan Kline <jordan.kline@gmail.com>
2018-03-10 17:56:32 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(categories_hashes)
+  	categories_hashes.each do | i, category_attributes |
+  	  unless category_attributes[:name] == ""
+  		category = Category.find_or_create_by(:name => category_attributes[:name])
+  		# self.categories << category
+	  	  if !self.categories.include?(category)
+	  		self.post_categories.build(:category => category)
+	  	  end
+	  end
+  	end
+  end
 
 end

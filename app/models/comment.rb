d58bce8d1de76b6301db61ca5f2164a84050de35
independnt <gonzalezdennis6@gmail.com>
2018-03-17 16:30:00 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc{|attribute| attribute[:username].blank?}
+
+  def user_attributes(user_hash)
+      user = User.find_or_create_by(user_att)
+      if user.username != nil
+        self.user = user
+      end
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,18 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+
+  def categories_attributes=(categories_hashes)
+    categories_hashes.values.each do |category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attributes)
+        if !self.categories.include?(category)
+          self.categories << category
+        end
+      end
+    end
+  end
 
 end

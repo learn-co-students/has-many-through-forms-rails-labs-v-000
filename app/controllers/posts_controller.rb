9573d83a09b206e129516d9492515a0732fe91ed
lsivak <https://github.com/lsivak>
2018-03-18 08:52:27 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,14 @@ class PostsController < ApplicationController
 
   def new
 		@post= Post.new
-    
+
 	end
 
   def create
     post = Post.create(post_params)
+   #  @post.comments.build(content: params[:content])
+   # @post.comments.build(user_id: params[:user_id])
+   # @post.comments.build(post_id: params[:post_id])
     redirect_to post
   end
 

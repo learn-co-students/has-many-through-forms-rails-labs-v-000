72281d4161763a9a62506a35c5471ae3ca0e5013
remclay <remmy.lauren@gmail.com>
2018-03-23 12:51:55 +1000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @users = @post.comments.collect {|c| c.user }.uniq
+    @user = User.new
+    @comment = Comment.new
   end
 
   def index
@@ -9,16 +12,18 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @comment = Comment.new
+    @user = User.new
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], user_attributes: [:username, :email])
   end
 end

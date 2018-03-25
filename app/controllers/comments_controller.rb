014217dbaa6720d229eee3582941c9301b652ad0
jacobsilver2 <jacobsilver2@mac.com>
2018-03-25 09:40:33 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,9 +7,6 @@ class CommentsController < ApplicationController
 
   def create  
     comment = Comment.create(comment_params)
-    binding.pry
-    comment.user.build
-    comment.save
     redirect_to comment.post
   end
 

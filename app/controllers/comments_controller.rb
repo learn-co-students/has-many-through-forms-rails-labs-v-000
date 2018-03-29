9bfe8226919e3d4b49b2222584a9d7667059292f
Sean Clifford <sclifford6262@gmail.com>
2018-03-29 19:14:38 +0000

    saving in caseI lose WiFi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 class CommentsController < ApplicationController
 
   def create
+    #raise params.inspect
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
+require 'pry'
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(input_hash)
+    input_hash.values.each do |input|
+      self.user = User.find_or_create_by(username: input) unless input.blank?
+    end
+  end
+  
 end

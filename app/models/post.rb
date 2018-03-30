be9ad62a35e28fc3b8acb60f5673b4ffa025d508
Sean Clifford <sclifford6262@gmail.com>
2018-03-30 06:32:08 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
 
   def categories_attributes=(array_of_hashes)
     array_of_hashes.values.each do |hash|
-      self.categories << Category.find_or_create_by(hash) unless hash[:name].blank?
+      unless self.categories.include?(Category.find_by(hash)) || hash[:name].blank?
+      #  binding.pry
+        self.categories << Category.find_or_create_by(hash)
+      end
     end
   end
-
 
 end

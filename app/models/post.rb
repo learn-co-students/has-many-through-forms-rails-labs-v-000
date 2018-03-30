65346eb24f2d15608e10b6c2e1276ad0f678e0a4
Sean Clifford <sclifford6262@gmail.com>
2018-03-30 04:16:39 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,17 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(array_of_hashes)
+    array_of_hashes.values.each do |hash|
+      self.categories << Category.find_or_create_by(hash) unless hash[:name].blank?
+    end
+  end
 
 
 end

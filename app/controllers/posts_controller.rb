49e67650957b82f0fc0363b2f06200f62e1ae003
Chris Henry <cdhenry@gmail.com>
2018-03-31 22:35:31 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    commenters = @post.comments.collect {|comment| comment.user}
+    @commenters = commenters.uniq
   end
 
   def index
@@ -16,9 +19,18 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :show
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comments_ids:[], comments_attributes: [:content])
   end
 end

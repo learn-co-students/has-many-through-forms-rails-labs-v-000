bc5572c15aad05356b87900d9304cb32fe4d51f6
ingridwong0715 <ingrid830715@gmail.com>
2018-04-03 16:07:47 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,20 @@
 class CommentsController < ApplicationController
 
   def create
+       # {"utf8"=>"✓", "comment"=>{"user_id"=>"1", "post_id"=>"1", "user_attributes"=>{"username"=>""}, "content"=>"great post!"}, "commit"=>"Create Comment", "controller"=>"comments", "action"=>"create"}
     comment = Comment.create(comment_params)
+
     redirect_to comment.post
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(
+      :content,
+      :post_id,
+      :user_id,
+      user_attributes: [:username]
+      )
   end
 end

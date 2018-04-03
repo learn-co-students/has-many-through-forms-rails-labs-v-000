d9f1af8dc07e2aa4d8fcadff9b726ce166d873fe
Caroline Shields <cbshields@outlook.com>
2018-04-03 00:38:39 +0000

    new user in posts/show.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cat1 = Category.create!(name: "Funny")
+cat1.posts.create!(title: "What I did last summer", content: "Here is content1")
+cat1.posts.create!(title: "What you did last year", content: "Here is content1")
+
+cat2 = Category.create!(name: "Cool")
+cat2.posts.create!(title: "What I did last summer", content: "Here is content1")
+cat2.posts.create!(title: "What you did last year", content: "Here is content1")
+cat2.posts.create!(title: "What we did in the Hamptons", content: "Here is content1")
+
+cat3 = Category.create!(name: "Work")
+cat3.posts.create!(title: "Why I need a new job", content: "Here is content1")
+
+comment1 = Comment.create!(content: "I thought this was not very good")
+comment2 = Comment.create!(content: "Nice Job")
+comment3 = Comment.create!(content: "You should try it this way")
+comment4 = Comment.create!(content: "Sweet bra!")
+comment5 = Comment.create!(content: "You should try the Carribean")
+comment6 = Comment.create!(content: "try going to this site instead")
+comment7 = Comment.create!(content: "Cool!")
+
+post1 = Post.create!(title: "What I did last summer", content: "Here is content1")
+post2 = Post.create!(title: "What you did last year", content: "Here is content1")
+post3 = Post.create!(title: "What we did in the Hamptons", content: "Here is content1")
+post4 = Post.create!(title: "Why I need a new job", content: "Here is content1")
+
+post1.comments.create!(content: "I thought this was not very good")
+post2.comments.create!(content: "Nice Job")

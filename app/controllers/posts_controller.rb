adf74ae7568e50436309d8d5fbe5a8d5f4f20a6d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-11 16:59:13 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment= @post.comments.build
+#    binding.pry
+
   end
 
   def index
@@ -19,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], users_attributes: [:name], comments_attributes: [:content])
   end
 end

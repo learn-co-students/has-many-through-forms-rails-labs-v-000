5ea228f1d5b3be7ac1beffba791d18276ac096fe
github username <github email address>
2018-04-12 23:57:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(set_of_attributes)
+    set_of_attributes.values.each do |attribute|
+      category = Category.find_or_create_by(attribute)
+      self.categories << category
+    end
+  end
+
+  def users_who_commented
+    self.comments.map(&:user).compact.uniq.map(&:username)
+  end
 end

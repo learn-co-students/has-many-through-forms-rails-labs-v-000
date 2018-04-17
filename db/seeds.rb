455d49ab795d25182d3af09d4e11963cc8fc748e
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-04-17 15:45:41 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "This is a Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+Post.create(title: "This is another Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+Post.create(title: "This is a Third Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+
+User.create(username: "User1", email: "user1@email.com")
+User.create(username: "User2", email: "user2@email.com")
+User.create(username: "User3", email: "user3@email.com")
+
+Category.create(name: "Education")
+Category.create(name: "Science")
+Category.create(name: "Cooking")
+
+PostCategory.create(post_id: 1, category_id: 1)
+PostCategory.create(post_id: 2, category_id: 2)
+PostCategory.create(post_id:3, category_id:3)
+
+Comment.create(user_id: 1, post_id: 2, content: "Great post!")
+Comment.create(user_id: 2, post_id: 3, content: "Awesome job!")
+Comment.create(user_id: 3, post_id: 1, content: "You rock!")

5b4246d643a749a5bf32923c4b47116c10ef4e26
nptravis <nptravis@yahoo.com>
2018-04-20 00:16:13 +0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  # def user_attributes=(attribtutes)
+  # 	binding.pry
+  # end
 
 end

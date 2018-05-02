737acd5ac7a7b5f06af11e928decd3f9dd79b609
Kelsey Fecho <kfecho30@gmail.com>
2018-05-02 22:46:55 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :reject
 
+  def reject(attributes)
+    attributes['username'].blank?
+  end
 end

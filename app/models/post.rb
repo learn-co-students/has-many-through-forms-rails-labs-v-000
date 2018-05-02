8b598e2c97b16ff416e9e3da63b995ea3eaff20e
github username <github email address>
2018-05-02 10:21:41 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories
+
+  # def categories_attributes=(categories_attributes)
+  #   categories_attributes.each do |categories_attributes|
+  #     self.categories.build(categories_attributes)
+  #   end
+  # end
 
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end

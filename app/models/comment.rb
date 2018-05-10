59529b5cc431fd871bbaeba4b9681b5af823219d
Sam Kalum <stkalum@gmail.com>
2018-05-10 16:13:21 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def user_name
+    self.user.username
+  end
 end

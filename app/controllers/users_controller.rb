1869fbe5ed8c6ec8be1b06e15fe9d7d1e57af69e
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-14 18:58:11 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @post_ids = []
+    @user.comments.each do |comment|
+      @post_ids << comment.post_id
+    end
+    binding.pry
+    @post_ids.each do |post_id|
+      post = Post.find_by(post_id)
+      @posts << post
+    end
+
   end
 
 end

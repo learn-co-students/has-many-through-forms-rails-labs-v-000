270e236abf6de517391455f494df15dbecbda18c
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-20 19:28:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments, :reject_if => proc { |attributes| attributes['username'].blank? }
+  accepts_nested_attributes_for :users, :reject_if => proc { |attributes| attributes['username'].blank? }
 
-  def users_attributes=(user_attributes)
+  def user_attributes=(user_attributes)
       user_attributes.values.each do |user_attribute|
         user = User.find_or_create_by(user_attribute)
         self.users << user

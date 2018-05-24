d7f3fe19b31c9aa5bb12277d509bf549c8a21549
ElleeB <laura.trager@gmail.com>
2018-05-24 11:06:58 -0400

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user_1 = User.create(username: "Bob")
+user_2 = User.create(username: "Sandra")
+user_3 = User.create(username: "Jo")
+
+
+category_1 = Category.create(name: "funny")
+category_2 = Category.create(name: "sad")
+category_3 = Category.create(name: "happy")
+
+
+comment_1 = Comment.new(content: "This is the best post I've ever read")
+comment_1.user_id = 1
+comment_1.save
+
+comment_2 = Comment.new(content: "What a horrible piece of writing!")
+comment_2.user_id = 3
+comment_2.save
+
+comment_3 = Comment.new(content: "Yum Yum Yum Yum!")
+comment_3.user_id = 2
+comment_3.save
+
+
+post_1 = Post.new(title: "Muffins Are Yummy", content: "muffins muffins muffins muffins muffins muffins muffins")
+post_1.categories << category_1 << category_2 << category_3
+post_1.comments << comment_3
+post_1.save
+
+post_2 = Post.new(title: "I Like Ice Cream", content: "ice cream ice cream ice cream ice cream ice cream ice cream ice cream!")
+post_2.categories << category_1 << category_3
+post_2.comments << comment_1
+post_2.save
+
+post_3 = Post.new(title: "Chocolate Chocolate Chocolate", content: "chocolate is the greatest of all the things chocolate is the greatest of all the things chocolate is the greatest of all the things chocolate is the greatest of all the things")
+post_3.categories << category_3
+post_3.comments << comment_2
+post_3.save

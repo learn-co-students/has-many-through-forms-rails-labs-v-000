d4107647d5dedd7b794f537e149cc3318662a799
ElleeB <laura.trager@gmail.com>
2018-05-29 18:05:30 -0400

    Passing all but 'comments' tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,3 +36,31 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content, comments_attributes:[:content, :user], category_ids:[], categories_attributes: [:name])
   end
 end
+
+# 
+# end
+#
+# def create
+# #   "post"=>
+# # {"title"=>"I love cookies.",
+# #  "content"=>"C is for Cookie, that's good enough for me!",
+# #  "category_ids"=>["3", ""],
+# #  "categories_attributes"=>{"0"=>{"name"=>"delicious"}}}
+#   @post = Post.create(post_params)
+#
+#   redirect_to post_path(@post)
+# end
+#
+# def update
+#   # "comments"=>{"users"=>"3", "comment_content"=>"asdfa wet34 twesgarsdfgcv"}
+#   @post = Post.find(params[:id])
+#   binding.pry
+#   # if params[:username] create new user with username that belongs to the comment
+#   # if [:comment_content], create new comment that belongs to the post
+#   # "post"=>{"user_ids"=>["1", ""], "users_attributes"=>{"0"=>{"username"=>"Laura"}
+#
+#   # params[:post][:users_attributes] => {"0"=>{"username"=>"Laura"}}
+#   # params[:post][:user_ids] =>["1", "2"...]
+#   @post.update(post_params)
+#
+#   redirect_to post_path(@post)
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
@@ -11,12 +12,4 @@ class Post < ActiveRecord::Base
       self.categories << category
     end
   end
-
-  # def comments_attributes=(comment_attributes)
-  #   comment_attributes.values.each do |comment_attribute|
-  #     comment = Comment.find_or_create_by(comment_attribute)
-  #     self.comments << comment
-  #   end
-  # end
-
 end

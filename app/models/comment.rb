385dd09a7d112858983c6fa2d22b0e84a61162ae
Kaitlin Kelly <kk504408@gmail.com>
2018-05-29 00:50:48 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.comment_user.build(user: user)
+    end
+  end
+
 end

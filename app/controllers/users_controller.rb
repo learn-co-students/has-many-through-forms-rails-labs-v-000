d0e8d56c45a9d0252d737a96b734a4086006481c
github username <github email address>
2018-06-02 13:08:09 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id]) unless @user.valid?
+    @user = User.find(params[:id])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,4 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
-
-  validates_uniqueness_of :username
-
-
-  # def categories_attributes=(category_attributes)
-  #   category_attributes.values.each do |category_attribute|
-  #     category = Category.find_or_create(category_attribute)
-  #     self.categories << category
-  #   end 
-  # end
-
 end

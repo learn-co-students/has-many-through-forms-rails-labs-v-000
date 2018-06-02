b85543a7cf5d61b1de9b58ee4ef17cc582a7fb58
allisonkadel <akadel01@gmail.com>
2018-06-02 19:33:52 -0400

    save

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,28 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  #accepts_nested_attributes_for :categories -- this macro writes te below method (without additional logic)
+
+  def categories_attributes=(categories_hashes)
+
+    categories_hashes.each do |index, category_attrs|
+
+      if category_attrs[:name].present?
+
+        category = Category.find_or_create_by(name: category_attrs[:name])
+        
+        if !self.categories.include?(category)
+
+          self.post_categories.build(:category => category)
+
+        end
+
+      end
+
+    end
+
+
+  end
+
 
 end

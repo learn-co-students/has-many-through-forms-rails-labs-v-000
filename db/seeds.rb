d10dbf9d0e05da0ddc30c4f75659218580c190d1
github username <github email address>
2018-06-06 01:02:59 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-kim = User.create!(username:"Kim", email: "Test@test.com")
-randi = User.create!(username:"Randi", email: "Test1@test.com")
-skip = User.create!(username: "Skip", email: "Test2@test.com")
-jess = User.create!(username:"Jess", email: "Test3@test.com")
-
-kimsposts = [
-    Post.create!(title: "Scary!", content: "post 1content"),
-    Post.create!(title: "5 + 5 = 10", content: "post 2content"),
-    Post.create!(title: "The Human Brain is awesome", content: "post 3content")
-]
-
-kim.posts << kimsposts
-
-suspense = Category.create!(name: "Suspense")
-psych = Category.create!(name: "Psychology")
-biology = Category.create!(name: "Biology")
-math = Category.create!(name: "Math")
-
-post_cat = PostCategory.create!(post_id: 1, category_id: 1)
-post_cat2 = PostCategory.create!(post_id: 2, category_id: 4)
-post_cat3 = PostCategory.create!(post_id: 3, category_id: 2)
-
-kcomment = Comment.create!(content:"What's that about?", user_id: 1, post_id: 3)
-racomment = Comment.create!(content:"I love this book", user_id: 2, post_id: 3)

70b748876affd2e11e9b44ac4ce58af7c92347d1
Aly Alidina <aly_alidina@hotmail.com>
2018-06-09 14:11:51 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  #when we do mass assignment, our Post model will call a method #called categories_attributes=
   accepts_nested_attributes_for :categories
+
+  def accepts_nested_attributes_for=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end

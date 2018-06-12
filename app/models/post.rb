d0c6befe54ce9c6fd4592c3201fd2ee78a5b55f6
Aly Alidina <aly_alidina@hotmail.com>
2018-06-12 16:53:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,14 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   #when we do mass assignment, our Post model will call a method #called categories_attributes=
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+  def categories_attributes=(category_hashes)
+    category_hashes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
     end
   end
 end

c92435e3ecfb2c33e9d05c4d4414689ab990f948
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 21:45:16 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,18 @@
+require 'pry'
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: proc { |attr| attr[:username].blank? }
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
-  def user_attributes=(user_attributes)
-    user_attributes.values.each do |user_attr|
-      user = User.find_or_create_by(username: user_attr)
-      self.user = user if self.user.nil?
-    end
-  end
+#  def user_attributes=(user_attributes)
+#    user_attributes.each do |user_attr|
+#      binding.pry
+#      if user_attr[:username].present?
+#        user = User.find_or_create_by(username: user_attr)
+#        self.user = user if self.user.nil?
+#      end
+#    end
+#  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
-  accepts_nested_attributes_for :comments, reject_if: :fields_blank?
 
   def categories_attributes=(categories_attributes)
     categories_attributes.values.each do |cat_attr|
-      category = Category.find_or_create_by(cat_attr)
-      self.categories << category
+      if cat_attr[:name].present?
+        category = Category.find_or_create_by(cat_attr)
+        if !self.categories.include?(category)
+          self.post_categories.build(category: category)
+        end
+      end
     end
   end
 

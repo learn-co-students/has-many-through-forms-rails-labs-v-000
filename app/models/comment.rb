a35bcbef7d6d63549dd06c3daa70e1d159f06fab
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 19:09:30 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attr| attr[:username].blank? }
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attr|
+      user = User.find_or_create_by(username: user_attr)
+      self.user = user if self.user.nil?
+    end
+  end
 
 end

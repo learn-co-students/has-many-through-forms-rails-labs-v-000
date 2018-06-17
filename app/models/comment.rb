e867b8e1db5cb9a4cfadc14ac6e90ca3797f55f2
Gingertonic <bethmschofield@gmail.com>
2018-06-17 13:02:09 +0930

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user
+  # accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['name'].blank? }
 
   # def user_attributes=(user_attributes)
   #   user = User.find_or_create_by(user_attributes)
+
+  def user_attributes=(user_attributes)
+    # raise user_attributes.inspect
+      if user_attributes[:username] != ""
+        user = User.find_or_create_by(user_attributes)
+        self.user = user
+      end
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  # accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
   def categories_attributes=(category_attributes)
-    # byebug
     category_attributes.values.each do |attribute|
-      category = Category.find_or_create_by(attribute)
-      self.post_categories.build(category: category)
+      if attribute[:name] != ""
+        category = Category.find_or_create_by(attribute)
+        self.post_categories.build(category: category)
+      end
     end
   end
 

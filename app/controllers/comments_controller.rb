9e1278926b2c1fbaca1c886cce99027676aea890
Wali Rahman <wali.rahman89@yahoo.com>
2018-06-27 12:56:42 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
 
-  
+
   def create
     @comment = Comment.create(comment_params)
-    @user = User.new
-    @user.email 
+    redirect_to post_path(@comment.post)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username], user_attributes: [:email])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username, :email])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to post
+    redirect_to @post
   end
 
   private

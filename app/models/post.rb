2ca1269839bd5dad52aca8922e9786da0e073004
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-12 19:50:38 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(cat_hashes)
+    cat_hashes.each do |i, cat_attr|
+      category = Category.find_or_create_by(name: cat_attr[:name])
+      self.categories << category
+    end
+  end
+
 
 end

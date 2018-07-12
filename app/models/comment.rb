9fdb88c0503f1191f526e7fb44bd4a56bd2d2690
alyssa0528 <alyssa.kim@gmail.com>
2018-07-12 10:20:39 +1000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
+  #def user_attributes=(user_attributes)
+  #  self.username = User.find_or_create_by(user_name: user_attributes[:username])
+  #end
+
+  #def user_attributes
+  #  self.username ? self.username : nil
+  #end
 end

4773c9e214f644b34b1d421b8cc090135f985ef7
Jul Jen <juliajen.prog@gmail.com>
2018-07-19 23:55:23 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @commment = @post.comments.build
   end
 
   def index
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  # accepts_nested_attributes_for :categories
-  # accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|

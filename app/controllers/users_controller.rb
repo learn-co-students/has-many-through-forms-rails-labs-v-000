cc6783d98db72a41afec9a7622f2fc4e11870804
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-07-19 13:22:14 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-#   def create
-#     @user = User.create(user_params)
-#     redirect_to post_path(@user.post)
-#   end
-#
-# private
-#   def user_params
-#     params.require(:user).permit(:username, :email)
-#   end
-
 end

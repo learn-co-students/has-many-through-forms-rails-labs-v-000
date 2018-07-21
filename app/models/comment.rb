58249814d3cfb404931116a4fc2193af0cb707c1
goatcheese <nonsquishy@gmail.com>
2018-07-21 12:56:49 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :username?
+
+  def username?(field)
+    field[:username].empty?
+  end
 end

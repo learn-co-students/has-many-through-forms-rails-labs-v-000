cf6e96382b095df4d2202aa43aae9642c13ecb9e
GabrielValle91 <gvalle91@gmail.com>
2018-07-20 20:42:22 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :reject_users
+
+
+  def reject_users(attributes)
+    attributes[:username].blank?
+  end
 
 end

189bc8d70fd2deb6e5f4bc953cdaf4394e3cd31c
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-23 11:21:02 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc {|attributes| attributes["username"].blank? }
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 
 end

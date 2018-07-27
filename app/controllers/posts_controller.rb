f97022c75c7f5d7f48bf67e8adde610ab06a946f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-27 02:00:03 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[],
+    params.require(:post).permit(:title, :content, :category_ids=>[],
       categories_attributes: [:name],
       comments_attributes: [:user_id, :content],
       users_attributes: [:username])

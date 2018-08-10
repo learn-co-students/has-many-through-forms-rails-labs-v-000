c5e2242dbc400f05e2a667b42621b73bc7df4a32
learn <albazaz@gmail.com>
2018-08-10 08:04:36 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
+require 'pry'
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @unique_users = @post.users.uniq
+    #binding.pry
+  @post.comments.build
+  @post.save
+    #  redirect_to comment.post
+    #end
   end
 
   def index
@@ -14,11 +21,16 @@ class PostsController < ApplicationController
   def create
     post = Post.create(post_params)
     redirect_to post
+
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end

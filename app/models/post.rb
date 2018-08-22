5636770de5d8e5b45dfc25e78759ca87261d3493
Kayvan Vakili <kvakili813@gmail.com>
2018-08-22 23:38:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,23 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(cat_hashes)
+      cat_hashes.each do |i, cat_attr|
+        if cat_attr[:name].present?
+          category = Category.find_or_create_by(name: cat_attr[:name])
+          if !self.categories.include?(category)
+            # self.categories << category #returns all categories, we'd rather insert a new row in the post_category table to associate them
+            self.post_categories.build(category: category)
+          end
+        end
+      end
+    end
+
+    def comments_attributes=(comments_attributes)
+      byebug
+      comments_attributes.each do |i, comment_attributes|
+        self.comments.build(comment_attributes)
+      end
+    end
 
 end

c00e2e77268b5698075d06fe203112a117f6cb8f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-23 15:53:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :comments, :users
+  
+  accepts_nested_attributes_for :comments, :users, reject_if: :empty_fields?
+  
+  def empty_fields?
+    binding.pry
+    # attributes['title'].blank?
+  end
 
   def commenters
     commenters = []

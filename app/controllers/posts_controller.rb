04ebcdf37fceaeda6b188a68dba13a42e2fbb4ff
Kachel Wittig <kjwittig@gmail.com>
2018-09-02 00:05:16 -0500

    forms created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,13 @@ class PostsController < ApplicationController
 
   private
 
+# remember that post.category_ids = [1] is the same as
+# post.categories = [Category.find(1)]
+# collection_singular_ids=ids
+# Replace the collection with the objects identified by the primary keys in ids.
+# This method loads the models and calls collection=.
+
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, :categories_attributes => [:name], :category_ids => [] )
   end
 end

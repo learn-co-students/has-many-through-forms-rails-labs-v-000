0ceac40a2ca230148bca34b9c94b7cc2e79e027d
theresamorelli <theresa.l.morelli@gmail.com>
2018-09-01 21:32:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      category = Category.find_or_create_by(category_attributes)
+      # self.categories << category
+      self.post_categories.build(:category => category)
+    end
+  end
 end

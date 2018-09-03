9e7d6d2c8365126ed90a9332a41664fa7002e33d
github username <github email address>
2018-09-02 17:53:05 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  # def user_attributes=(user_attributes)
+  #   user_attributes.values.each do |user_attribute|
+  #     post = Post.find(self.post_id)
+  #     self.user = User.find_or_create_by(username: user_attribute)
+  #       User.all << self.user
+  #      self.user
+  #       #binding.pry
+  #   end
+  # end
 
 end

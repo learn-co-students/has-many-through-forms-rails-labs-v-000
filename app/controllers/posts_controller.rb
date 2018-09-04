0f1d05af82a63b66c19d83a7a716e039f802cfd9
Kachel Wittig <kjwittig@gmail.com>
2018-09-04 13:52:10 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    2.times { @post.comments.build }
+    # here we went ahead did the association already!
+    @comment = @post.comments.build
   end
 
   def index
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(post_params)
-    redirect_to post
+    redirect_to post_path(post)
   end
 
   private

730f93bc06a761fd2a3894df3db947f5e0e93fb2
github username <github email address>
2018-09-04 02:21:04 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    if user_attributes[:username] != ""
+      self.build_user(user_attributes)
+    end
+  end
 end

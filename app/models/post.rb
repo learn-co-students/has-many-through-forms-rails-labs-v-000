4855b6187058f7e01212aeea923d68c93c83a7f0
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-09 04:32:17 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+  def users_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.post_users.build(user: user)
+    end
+  end
+  
 end

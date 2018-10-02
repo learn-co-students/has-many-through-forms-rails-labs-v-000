c3cc2602602e95e17e57cee842abd33ccff0d793
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-02 00:43:06 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
+  #  raise params.inspect
     @comment = Comment.create(comment_params)
   #  post = Post.find(comment_params[:user_id])
   #  post.comments << @comment
+  #  post.save
+
     redirect_to @comment.post
   end
 

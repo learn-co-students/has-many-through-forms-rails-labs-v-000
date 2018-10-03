eecb62b76db9c8490d7b5532b7ba57949f6ef65a
Christopher P. Dundon <chris.dundon@gmail.com>
2018-10-03 00:23:09 -0400

    about 4 hours of work - making progress - tough lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,22 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+		post = Post.create(post_params)
+    redirect_to posts_path
   end
 
 	def update
-
+		
+		username_ = params[:post][:users_attributes]["0"][:username].strip
+		if username_ != ''
+			new_user_ = User.new()
+			new_user_.username = username_
+			new_user_.save
+			uid_ = new_user_.id		
+		end
+		
+		params[:post][:comments_attributes]["0"][:user_id] = new_user_.id.to_s if new_user_
+	
 		post = Post.find_by_id(params[:id])	
 		post.update(post_params)
 		redirect_to post_path(post)
@@ -30,6 +40,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, comments_attributes: [:content, :user_id])
+    params.require(:post).permit(:title, :content, category_ids:[], comments_attributes: [:content, :user_id])
   end
 end

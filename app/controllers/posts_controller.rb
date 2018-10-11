0c7d118136a0fb1c5c12f5131c4bf95abb887531
Anu Khambete <anukhambete@gmail.com>
2018-10-11 13:22:16 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+
+require 'pry'
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
@@ -12,6 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
+    #binding.pry
     post = Post.create(post_params)
     redirect_to post
   end

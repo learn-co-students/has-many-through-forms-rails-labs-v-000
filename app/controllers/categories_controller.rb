666fb8f5bef482600fcb0f088aae0ced2a9ea9a8
vickicello <vicki.cello@gmail.com>
2018-10-14 11:44:50 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController
-
   def show
     @category = Category.find(params[:id])
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.create(category_params)
+    redirect_to category
   end
 end

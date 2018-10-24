2a6c294878d1980de163d9904c0090a3413279cb
FionaDL <github email address>
2018-10-23 22:18:17 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+  end
+
   def create
     comment = Comment.create(comment_params)
-    redirect_to comment.post
+    redirect_to post_path(comment.post)
   end
 
   private

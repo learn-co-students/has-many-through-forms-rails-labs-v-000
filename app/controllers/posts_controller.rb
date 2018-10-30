1311648f5f79c91b9ef974791cfc276e70f9feaf
ryanmanchester <ryanm17@gmail.com>
2018-10-30 16:21:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    
+    @comment = Comment.new
   end
 
   def index
@@ -15,8 +15,8 @@ class PostsController < ApplicationController
 
   def create
     #byebug
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private

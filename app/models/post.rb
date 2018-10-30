88d618490b063c929f3015ec67f3f7c674041b65
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-10-30 10:15:51 -0400

    Passing 6 tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
+  
+  # def categories_attributes=(category_attributes)
+  #   self.categories += category_attributes.values.each do |category_attribute|
+  #     Category.find_or_create_by(category_attribute)
+  #   end
+  # end
+
+  # def categories_ids=(ids)
+  #   self.categories << ids
+  # end
 end

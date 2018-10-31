41e6e20723f48ac0a276a811a573285a29a13a1b
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-10-30 22:00:09 -0400

    Almost done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,20 +5,8 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+    self.categories << category_attributes.values.collect do |category_attribute|
+      Category.find_or_create_by(category_attribute)
     end
   end
-
-  
-  # def categories_attributes=(category_attributes)
-  #   self.categories += category_attributes.values.each do |category_attribute|
-  #     Category.find_or_create_by(category_attribute)
-  #   end
-  # end
-
-  # def categories_ids=(ids)
-  #   self.categories << ids
-  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+content = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC."
+
+users = User.create([
+  { username: 'arye' },
+  { username: 'dov' }
+])
+
+categories = Category.create([
+  { name: 'viral' },
+  { name: 'cats' }
+])
+
+posts = Post.create([
+  { title: 'post 1', content: content, categories: categories },
+  { title: 'post 2', content: content, categories: [categories[1]] }
+])

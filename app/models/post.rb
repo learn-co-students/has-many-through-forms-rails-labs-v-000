8400faa53d3c56293a69bc965ee804dd935851df
David M <32019560+dmcgrann@users.noreply.github.com>
2018-11-02 17:19:31 -0400

    Update post.rb
    Added alternative to collection<< method.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,9 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      self.categories << category #this will list all categories when saving
+      #preferred =
+        #self.post_categories.build(:category => category)
     end
   end
 end

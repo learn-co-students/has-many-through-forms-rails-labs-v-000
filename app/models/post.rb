9ae91f4cd90acc72117d75066a432c3791a7ec4b
David M <32019560+dmcgrann@users.noreply.github.com>
2018-11-02 17:20:04 -0400

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,7 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category #this will list all categories when saving
-      #preferred =
-        #self.post_categories.build(:category => category)
+      self.categories << category
     end
   end
 end

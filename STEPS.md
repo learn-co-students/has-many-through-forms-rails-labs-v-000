aa81fbcca3477d71f938e392bedfa898e223c21a
freeWillee <will.i.lee@outlook.com>
2018-11-06 21:38:53 -0700

    Done.

---
+++ STEPS.md
@@ -0,0 +1,20 @@
+STEPS:
+1. Post have many categories / categories have many posts
+2. Post nested form with smart non-duplicate categories
+3. Posts have many comments
+4. A comment belongs to a user
+5. On post#show page --> need comment form with dropdown for users and a textbox for a username.
+6. Create a `users#show` page that links to all of the posts a user has commented on.
+7. Create a `categories#show` page that links to all of the posts associated with a given category.
+
+
+
+post_spec
+categories_spec
+comments_spec
+user_spec
+
+1. POST MODEL
+2. CATEGORY MODEL
+3. POST CONTROLLER
+4. POSTS/NEW
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,32 @@
 class Post < ActiveRecord::Base
-  has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :post_categories #methods are added
+    #post_catgories
+    #post_categories<<
+    #post_categories.delete
+    #collection_singular_ids ==> post_category_ids=ids
+
+  has_many :categories, through: :post_categories #methods are added
+    #category_ids
   has_many :comments
   has_many :users, through: :comments
+
+  def categories_attributes=(categories_hashes)
+    # {
+    #   "0"=>{"name"=>"new category 1 "}, 
+    #   "1"=>{"name"=>"new category 2"}
+    #   }
+
+    # how to create a category for each of the hashes inside the categories_hashes hash
+
+    categories_hashes.each do |i, category_attributes|
+      # need to create a category that is already associated with this post & need to make sure that this category doesn't already exist by name
+      category = Category.find_or_create_by(name: category_attributes[:name])
+      self.categories << category
+      self.post_categories.build(:category => category) 
+      # PICK UP @ 53:40
+      
+    end
+  end
 
 
 end

529607e35046492ec29f59a056122ba236232c7a
github username <github email address>
2018-11-19 15:05:08 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
-    @comment = Comment.new
   end
 
   def new

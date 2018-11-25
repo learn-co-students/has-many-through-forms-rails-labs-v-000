6be32e08ddf9ee7d4a6f10f5cdc78e8eedcbf5b6
Brian Birdwell <bbirdwell@gmail.com>
2018-11-25 06:57:27 +0000

    complete post spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
@@ -5,4 +6,12 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
+
+  def commenters
+    array = []
+    self.comments.each do |c|
+      array << c.user
+    end
+    array.uniq!
+  end
 end

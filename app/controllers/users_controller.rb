09b138bac8369ffbd875b4941c4774fef5603b14
rina71 <yurinas@gmail.com>
2018-11-27 11:21:49 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
   end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to comment.post
+  end
+
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username)
+    end
 
 end

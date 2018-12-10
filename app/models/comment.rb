060ba4f8b4fd8794bc3a379f32671616a6899fb0
github username <github email address>
2018-12-10 12:58:10 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,4 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :all_blank
-
-  def reject_user(attributes)
-   attributes[:username].blank?
-  end
 end

8215d0847c1f8f3fedc1dbd05743b0a31df3bba1
Taylor Williams <taylor@thewilliams.net>
2018-12-12 00:20:21 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-
+  def categories_attributes=(cat_at)
+    cat_at.values.each do |cat|
+      self.categories << Category.find_or_create_by(cat)
+    end
+  end
 end

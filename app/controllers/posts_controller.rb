0a5e735fa995ff0e0918dfcf4f8c6afa8ceb9ff1
mrjak13 <justinkenney13@gmail.com>
2018-12-14 12:21:41 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,8 @@ class PostsController < ApplicationController
   end
 
   def create
+    # raise post_params.inspect
+    # binding.pry
     post = Post.create(post_params)
     redirect_to post
   end

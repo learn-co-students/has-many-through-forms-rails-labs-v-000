bfff39498bb6f5b6ac2c9da77d4d5986f3144527
Mika88 <mikaspivak@gmail.com>
2019-01-04 01:30:02 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
+    @user = comment.user
   end
 
   private

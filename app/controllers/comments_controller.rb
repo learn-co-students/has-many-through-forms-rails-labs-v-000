5a9442c1d72a63e0cbfde815592014dbe102d5b2
hejeong <jonhong777@gmail.com>
2019-01-13 18:30:10 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,11 @@
 class CommentsController < ApplicationController
+  
 
   def create
+    if !params[:user_attributes][:username].empty?
+      user = User.create(username: params[:user_attributes][:username])
+      params[:comment][:user_id] = user.id
+    end
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end

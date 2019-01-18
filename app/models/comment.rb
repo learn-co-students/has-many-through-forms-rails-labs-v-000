436dfcdfc589316f8c8dd8b27af2b281615605a2
github username <github email address>
2019-01-18 13:25:37 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    #raise user_attributes.inspect
+    #binding.pry
+    if user_attributes[:username] != ""
+      user = User.find_or_create_by(username: user_attributes[:username])
+      self.user = user
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each_value do |category_attributes|
+      if category_attributes[:name] != ""
+        category = Category.find_or_create_by(category_attributes)
+        self.categories << category
+      end
+    end
+  end
 
 end

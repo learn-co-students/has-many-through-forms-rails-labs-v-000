4d0273e3b22a6a9b30dc8cb47f3e784d6d1eefd0
Michael Dilley <mike.dilley@gmail.com>
2019-01-22 11:15:08 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,22 +3,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def index
-    @posts = Post.all
-  end
-
   def new
     @post = Post.new
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to @post
   end
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
-  end
+    def post_params
+      params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    end
 end

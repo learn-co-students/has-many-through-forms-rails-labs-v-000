49315ffd4467eb53667f6761a0f6a6fb87d69e3b
harleyharl <harleymteden@gmail.com>
2019-02-07 12:19:02 -0800

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
+    # raise params.inspect
     @comment = Comment.create(comment_params)
-    redirect_to comment.post
+    redirect_to @comment.post
   end
 
   private
@@ -10,4 +11,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
 end

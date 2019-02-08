ddc8720e33e2f08a22d5d77608866e5b0336afee
Barbara Choy <barbchoy@gmail.com>
2019-02-08 22:16:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_hashes)
+    raise categories_hashes.inspect
+  end
+
 
 end

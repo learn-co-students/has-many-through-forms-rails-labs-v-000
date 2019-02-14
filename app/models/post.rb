e7a51729146d19544135fa7649c37b9d88b91cb7
Barbara Choy <barbchoy@gmail.com>
2019-02-14 19:21:07 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,12 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(categories_hashes)
     categories_hashes.each do |i, category_attributes|
-    category = Category.find_or_create_by(name: category_attributes[:name])
-    self.post_categories.build(:category => category)
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
     end
   end
 

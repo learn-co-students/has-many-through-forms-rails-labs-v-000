3f30ca438f28edc496105451acc82c242f3f33ff
Barbara Choy <barbchoy@gmail.com>
2019-02-15 22:43:53 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  
 end

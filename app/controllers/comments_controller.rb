44ba6c1e8613a2a6f76a11245b56d80d76bbc40c
Barbara Choy <barbchoy@gmail.com>
2019-02-16 06:15:12 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 
   def create
-    binding.pry
     comment = Comment.create(comment_params)
     redirect_to post_path(comment.post)
   end
@@ -9,7 +8,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    # params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    #params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,15 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_hashes)
+    user_hashes.each do |i, user_attributes|
+      if user_attributes[:username].present?
+        user = User.find_or_create_by(username: user_attributes[:username])
+        if self.user!=user
+          self.comment_user.build(:user => user)
+        end
+      end
+    end
+  end
+
 end

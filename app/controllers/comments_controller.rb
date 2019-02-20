62dfacbba25a71387ea3216e999349d29120f0f1
Erin Green <erin.y.green@gmail.com>
2019-02-20 16:37:51 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_id].blank?
+      params[:comment].delete(:user_id)
+    end
+
+    comment = Comment.create(comment_params)
+    redirect_to post_path(comment.post.id)
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :users, reject_if: proc { |attributes| attributes['username'].blank?}
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank?}
 
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
       if user_attributes[:username].present?
-        user = User.find_or_create_by(user_attribute)
+        user = User.find_or_create_by(user_attributes)
         if !self.user = user
           self.build_user(:username => user[:username])
         end

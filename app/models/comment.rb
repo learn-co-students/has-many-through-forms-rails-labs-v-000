0db53ef440af7ad9cb996ded4adf376899fe6445
ricecake88 <vocalise@gmail.com>
2019-03-19 20:25:54 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,20 +2,15 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user,
-                                reject_if: proc { |attributes| attributes.all? { |key, value| key == "_destroy" || value.blank? } }
+                                reject_if: -> (user_data) {user_data[:username].blank?}
                                 #:reject_if => :all_blank
                                 #reject_if: :reject_user, allow_destroy: true
 
 
-  def user_attributes=(user_attribute)
-    user = User.find_or_create_by(:username => user_attribute[:username])
-    self.user = user
-  end
-
-
-  def reject_user(attributes)
-    attributes['username'].blank?
-  end
+  #def user_attributes=(user_attribute)
+  #  user = User.find_or_create_by(:username => user_attribute[:username])
+  #  self.user = user
+  #end
 
 
 end

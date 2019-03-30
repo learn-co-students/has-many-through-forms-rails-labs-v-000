f14dab19c685a2d1a903f59b1adddd39fb4f3575
Leslie Stevens <flymaven@protonmail.com>
2019-03-30 10:20:13 -0500

    edit comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,20 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  validates_presence_of :user_username
 
   def user_attributes=(attributes)
     if !attributes[:username].empty?
+      #binding.pry
       self.user.username = User.find_or_create_by(username: attributes[:username])
+      binding.pry
     end
   end
 
     def user_username=(username)
-      self.user = User.find_by(username: username)
+      if username != nil
+        self.user = User.find_by(username: username)
+      end
     end
 
     def user_username

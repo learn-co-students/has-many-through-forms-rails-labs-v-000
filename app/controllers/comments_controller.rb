baf0cc0da29247a80735007f6422aa2669a52b10
Ashley <ashleywong678@gmail.com>
2019-04-12 18:14:25 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    # binding.pry
     comment = Comment.create(comment_params)
-    if comment.user.present?
-      comment.user = User.create(username: params[:comment][:user_attributes][:username])
-    end
     redirect_to comment.post
   end
 

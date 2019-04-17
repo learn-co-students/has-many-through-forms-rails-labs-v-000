5a2e38cef4b1a80beb9ed24e4125b3e7166ca56c
Jonathan Luk <jluk87@gmail.com>
2019-04-17 05:25:51 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,15 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  def user_attributes=(user_attributes)
+    new_user = User.new(user_attributes)
+    self.user = new_user
+  end
+
+  def user_attributes
+    self.user ? self.user.username : nil
+  end
 
 end

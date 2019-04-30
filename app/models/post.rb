154c5d97cfae43ab82e9f3dbac92697caf8c4fc2
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-29 22:15:26 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :users, reject_if: :all_blank
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|

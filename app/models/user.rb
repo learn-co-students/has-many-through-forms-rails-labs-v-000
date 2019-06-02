b0e1816711ee0cc0ef64eb5ab06ece4972836717
ahotchkin <ahotchk1@yahoo.com>
2019-06-02 19:28:13 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def user_comments
+    self.comments
+  end
+
+  def user_posts
+    self.posts.uniq
+  end
 end

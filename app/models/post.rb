c2aa79ef251995fb72aefa516c82814733ec7fd9
ahotchkin <ahotchk1@yahoo.com>
2019-06-03 14:55:33 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,14 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      # Will only create a category if a name is entered
+      if category_attribute[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        # Will only add a category to a post if the post doesn't already have that category
+        if !self.categories.include?(category)
+          self.post_categories.build(category: category )
+        end
+      end
     end
   end
 

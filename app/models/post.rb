0b54fef5482d47f019b9883c88c6d31061989641
github username <github email address>
2019-06-19 22:08:35 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,11 +15,11 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def comments_attributes=(comment_attributes)
-    comment_attributes.values.each do |comment_attribute|
-      comment = Comment.find_or_create_by(comment_attribute)
-      self.comment_attributes.build(comment: comment)
-    end
-  end
+  #def comments_attributes=(comment_attributes)
+   # comment_attributes.values.each do |comment_attribute|
+    #  comment = Comment.find_or_create_by(comment_attribute)
+     # self.comment_attributes.build(comment: comment)
+    #end
+  #end
 
 end

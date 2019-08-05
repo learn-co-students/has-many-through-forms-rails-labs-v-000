781c951f26f077c396fb8c893466312f1d3b53fa
github username <github email address>
2019-08-05 12:28:01 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,37 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :post_categories
+  accepts_nested_attributes_for :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.post_categories.build(category: category)
+    end
+  end
 
+  def comments_attributes=(comments_attributes)
+    comments_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments.build(comment: comment)
+    end
+  end
+
+  def unique_commenters
+    comment_users = []
+
+    self.comments.each do |comment|
+      comment_users << comment.user
+    end
+    
+    comment_users.uniq.compact
+    #compact removes nil
+  end
 end
+=begin
+
+   <% @post.unique_commenters.each do |commenter| %>
+    <%= commenter.user_name %>
+  <% end %>
+=end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+  accepts_nested_attributes_for :comments
+  
+
+  def comments_attributes=(comments_attributes)
+    comments_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments.build(comment: comment)
+    end
+  end
+  
 end
